7c97aba9501781f5b63cfd573e484a3d5dc348a7
Tom Russell <tomalrussell@gmail.com>
2018-05-30 14:31:23 +0100

    Script comments for OS data extract

---
+++ etl/extract_addressbase.py
@@ -0,0 +1,10 @@
+"""Extract address points from CSV in *.zip
+
+Relevant CSV columns::
+0 - UPRN
+1 - TOID (may not match given version of OS MasterMap)
+16 - OSGB Easting
+17 - OSGB Northing
+18 - Latitude
+19 - Longitude
+"""
---
+++ etl/extract_mastermap.py
@@ -0,0 +1,7 @@
+"""Extract buildings from *.gml.gz
+
+Features where::
+    descriptiveGroup = '(1:Building)'
+
+Use `fid` as source ID, aka TOID.
+"""
