d5654e4e1e8b67e7e73a24506a8aab3cb8fbfacb
Tom Russell <tomalrussell@gmail.com>
2018-08-01 17:58:02 +0100

    Beta site frontend as static files

---
+++ frontend/main.js
@@ -0,0 +1,94 @@
+/**
+ * Background map
+ */
+function map(){
+    var OS_API_KEY = 'NVUxtY5r8eA6eIfwrPTAGKrAAsoeI9E9';
+
+    var light = L.tileLayer('https://api2.ordnancesurvey.co.uk/mapping_api/v1/service/zxy/{tilematrixSet}/{layer}/{z}/{x}/{y}.png?key={key}', {
+        key: OS_API_KEY,
+        tilematrixSet: 'EPSG:3857',
+        layer: 'Light 3857'
+    });
+
+    var night = L.tileLayer('https://api2.ordnancesurvey.co.uk/mapping_api/v1/service/zxy/{tilematrixSet}/{layer}/{z}/{x}/{y}.png?key={key}', {
+        key: OS_API_KEY,
+        tilematrixSet: 'EPSG:3857',
+        layer: 'Night 3857'
+    });
+
+    var baseMaps = {
+        "Light": light,
+        "Dark": night
+    };
+
+    var map = L.map('map', {
+        layers: light,
+        zoomControl: false,
+        attributionControl: false,
+        maxZoom: 18,
+        minZoom: 7,
+        center: ([51.507222, -0.1275]),
+        zoom: 14,
+        doubleClickZoom: false,
+    });
+    
+    var attr = L.control.attribution({prefix: ""});
+    attr.addAttribution("Contains OS data © Crown copyright: OS Maps baselayers and building outlines. Building attribute data is © Colouring London contributors");
+    attr.addTo(map);
+    L.control.zoom({position: 'topright'}).addTo(map);
+    L.control.layers(baseMaps, {}, {position: 'topright'}).addTo(map);
+}
+
+/**
+ * Simple carousel
+ *
+ * Based on example code by Christian Heilmann
+ * http://christianheilmann.com/2015/04/08/keeping-it-simple-coding-a-carousel/
+ */
+carousel = function(){
+    var box = document.querySelector('.carousel');
+    if (!box) {
+        return
+    }
+    var next = box.querySelector('.next');
+    var prev = box.querySelector('.back');
+    // Define the global counter, the items and the
+    // current item
+    var counter = 0;
+    var items = box.querySelectorAll('.carousel-content li');
+    var amount = items.length;
+    var current = items[0];
+    box.classList.add('active');
+    // navigate through the carousel
+    function navigate(direction) {
+        // hide the old current list item
+        current.classList.remove('current');
+
+        // calculate the new position
+        counter = (counter + direction) % amount;
+        counter = counter < 0 ? amount - 1 : counter;
+        // set new current element
+        // and add CSS class
+        current = items[counter];
+        current.classList.add('current');
+    }
+    // add event handlers to buttons
+    next.addEventListener('click', function(ev) {
+        navigate(1);
+    });
+    prev.addEventListener('click', function(ev) {
+        navigate(-1);
+    });
+    // show the first element
+    // (when direction is 0 counter doesn't change)
+    navigate(0);
+}
+
+/**
+ * "Cut the mustard" and setup page
+ */
+if('querySelector' in document
+   && 'addEventListener' in window) {
+    carousel();
+    map();
+}
