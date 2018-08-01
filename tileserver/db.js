f46342af472c0f5cfa211615144545e4870420a9
Tom Russell <tomalrussell@gmail.com>
2018-08-01 18:42:09 +0100

    Remove frontend, api code from tile server

--- tileserver/db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg')
-const config = require('./config')
-
-const pool = new Pool(config.database)
-
-module.exports = {
-  query: (text, params) => pool.query(text, params)
-}
--- tileserver/index.js
+++ tileserver/index.js
@@ -8,9 +8,7 @@ const mapnik = require('mapnik')
 const SphericalMercator = require('@mapbox/sphericalmercator')
 
 // config file with connection details
-const config = require('./config')
-// db connection pool
-const db = require('./db')
+const config = require('../config.json')
 
 const TILE_SIZE = 256
 const TILE_BUFFER_SIZE = 64
@@ -27,47 +25,10 @@ if (mapnik.register_default_input_plugins) mapnik.register_default_input_plugins
 // set up app
 const app = express()
 
-// send static files if the match stuff in 'public'
-app.use(express.static('public'))
-
-// root is index.html
-app.get('/', (req, res) => res.sendFile(path.join(__dirname,'public', 'index.html')))
-
-// building geometry by lat/lon - TODO join geometry against buildings, move to apiserver
-app.get('/buildings', function(req, res){
-    const { lng, lat } = req.query
-    db.query(
-        `SELECT geometry_id
-        FROM geometries
-        WHERE
-        ST_Intersects(
-            ST_Transform(
-                ST_SetSRID(ST_MakePoint($1, $2), 4326),
-                3857
-            ),
-            geometries.geometry_geom
-        )
-        `,
-        [lng, lat]
-    ).then(function(data){
-        const rows = data.rows
-        if (rows.length){
-            res.send({geometry_id: rows[0].geometry_id})
-        } else {
-            res.status(404).send({error:'Not Found'})
-        }
-    }).catch(function(error){
-        console.error('Error:', error)
-        res.status(500).send({error:'Database error'})
-    })
-})
-
-// basic geometry tiles - TODO join against buildings and start parameterising with style
-app.get('/tiles/:z/:x/:y.png', function(req, res) {
+// basic geometry tiles
+app.get('/outline/:z/:x/:y.png', function(req, res) {
     const { z, x, y } = req.params
-    const { highlight } = req.query
-    const geometry_id = parseInt(highlight);
-    console.log(z, x, y, highlight)
+    console.log(z, x, y)
 
     const int_z = parseInt(z);
     const int_x = parseInt(x);
@@ -95,16 +56,6 @@ app.get('/tiles/:z/:x/:y.png', function(req, res) {
     layer.datasource = postgis;
     layer.styles = ['polygon'];
 
-    if (geometry_id){
-        var layer_h = new mapnik.Layer('highlight', PROJ4_STRING);
-        var conf_h = get_datasource_config()
-        conf_h.table = '(select * from geometries where geometry_id = '+geometry_id+') as highlight'
-
-        var postgis_h = new mapnik.Datasource(conf_h);
-        layer_h.datasource = postgis_h;
-        layer_h.styles = ['highlight'];
-    }
-
     map.load(
         path.join(__dirname, 'map_styles', 'polygon.xml'),
         { strict: true },
@@ -113,9 +64,66 @@ app.get('/tiles/:z/:x/:y.png', function(req, res) {
 
             map.add_layer(layer)
 
-            if(geometry_id){
-                map.add_layer(layer_h)
-            }
+            var im = new mapnik.Image(map.width, map.height)
+
+            map.extent = bbox
+            map.render(im, function(err, im) {
+                if (err) throw err
+
+                res.writeHead(200, {'Content-Type': 'image/png'})
+                res.end(im.encodeSync('png'))
+            });
+        }
+    )
+});
+
+// highlight single geometry
+app.get('/highlight/:z/:x/:y.png', function(req, res) {
+    const { z, x, y } = req.params
+    const { highlight } = req.query
+    const geometry_id = parseInt(highlight);
+    console.log(z, x, y, highlight)
+
+    const int_z = parseInt(z);
+    const int_x = parseInt(x);
+    const int_y = parseInt(y);
+
+    if (!int_x || !int_y || !int_z){
+        console.error("Missing x or y or z")
+        res.status(400).send({error:'Bad parameter'})
+        return
+    }
+
+    if(!geometry_id){
+        res.status(400).send({error:'No highlight'})
+    }
+
+    var bbox = mercator.bbox(
+        int_x,
+        int_y,
+        int_z,
+        false,
+        '900913'
+    );
+
+    var map = new mapnik.Map(TILE_SIZE, TILE_SIZE, PROJ4_STRING);
+    map.bufferSize = TILE_BUFFER_SIZE;
+
+    var layer_h = new mapnik.Layer('highlight', PROJ4_STRING);
+    var conf_h = get_datasource_config()
+    conf_h.table = '(select * from geometries where geometry_id = '+geometry_id+') as highlight'
+
+    var postgis_h = new mapnik.Datasource(conf_h);
+    layer_h.datasource = postgis_h;
+    layer_h.styles = ['highlight'];
+
+    map.load(
+        path.join(__dirname, 'map_styles', 'polygon.xml'),
+        { strict: true },
+        function(err, map){
+            if (err) throw err
+
+            map.add_layer(layer_h)
 
             var im = new mapnik.Image(map.width, map.height)
 
@@ -144,4 +152,4 @@ function get_datasource_config(){
     }
 }
 
-app.listen(3000, () => console.log('App listening on port 3000'))
+app.listen(8082, () => console.log('Tile server listening on port 8082'))
