473ab1fb0e329ef93f1cbb8680bcd6710b7db1aa
Tom Russell <tomalrussell@gmail.com>
2018-08-03 11:04:43 +0100

    Sketch a storeys layer

--- tileserver/index.js
+++ tileserver/index.js
@@ -102,7 +102,7 @@ app.get('/highlight/:z/:x/:y.png', function(req, res) {
     if(!geometry_id) res.status(400).send({error:'Bad parameter'})
     const bbox = get_bbox(req.params)
     const table_def = `(
-        select * from geometries 
+        select * from geometries
         where geometry_id = ${geometry_id}
     ) as highlight`
     const style_def = ['highlight']
@@ -119,19 +119,45 @@ app.get('/date_year/:z/:x/:y.png', function(req, res) {
     const bbox = get_bbox(req.params)
     // const table_def = 'geometries'
     const table_def = `(
-        SELECT 
+        SELECT
             cast(
                 b.building_doc->>'date_year'
                 as integer
             ) as date_year,
             g.geometry_geom
-        FROM 
-            geometries as g, 
+        FROM
+            geometries as g,
             buildings as b
-        WHERE 
+        WHERE
             g.geometry_id = b.geometry_id
     ) as outline`
     const style_def = ['date_year']
+    render_tile(bbox, table_def, style_def, function(err, im) {
+        if (err) throw err
+
+        res.writeHead(200, {'Content-Type': 'image/png'})
+        res.end(im.encodeSync('png'))
+    })
+});
+
+// date_year choropleth
+app.get('/size_storeys/:z/:x/:y.png', function(req, res) {
+    const bbox = get_bbox(req.params)
+    // const table_def = 'geometries'
+    const table_def = `(
+        SELECT
+            cast(
+                b.building_doc->>'size_storeys'
+                as integer
+            ) as size_storeys,
+            g.geometry_geom
+        FROM
+            geometries as g,
+            buildings as b
+        WHERE
+            g.geometry_id = b.geometry_id
+    ) as outline`
+    const style_def = ['size_storeys']
     render_tile(bbox, table_def, style_def, function(err, im) {
         if (err) throw err
 
