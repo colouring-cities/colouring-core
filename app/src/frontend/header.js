c1f35b024be3555f1f4692326f3e595416e3e9d6
Tom Russell <tomalrussell@gmail.com>
2018-10-02 21:48:13 +0100

    Link to pages from main nav

--- app/src/frontend/header.js
+++ app/src/frontend/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import Logo from './logo';
@@ -7,48 +7,50 @@ import './header.css';
 /**
  * Main Header
  */
-function Header(props) {
-    if (props.user) {
-        return (
-            <header className="main-header">
-                <nav className="navbar navbar-light navbar-expand-md">
-                    <Logo />
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <NavLink to="/map/date_year.html" className="nav-link">View Maps</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/about.html" className="nav-link">About</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/my-account.html" className="nav-link">My account (Logged in as {props.user.username})</NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        );
-    } else {
-        return (
-            <header className="main-header">
-                <nav className="navbar navbar-light navbar-expand-md">
-                    <Logo />
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <NavLink to="/map/date_year.html" className="nav-link">View Maps</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/about.html" className="nav-link">About</NavLink>
-                        </li>
+const Header = (props) => (
+    <header className="main-header">
+        <nav className="navbar navbar-light navbar-expand-md">
+            <Logo />
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <a className="nav-link" href="https://pages.colouring.london">Hello</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="https://pages.colouring.london/about">More about</a>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/map/date_year.html" className="nav-link">View Maps</NavLink>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="https://pages.colouring.london/buildingcategories">Building Categories</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="https://pages.colouring.london/whoisinvolved">Who's Involved?</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="https://discuss.colouring.london">Discuss</a>
+                </li>
+                {
+                    props.user?
+                    (
+                    <li className="nav-item">
+                        <NavLink to="/my-account.html" className="nav-link">My account (Logged in as {props.user.username})</NavLink>
+                    </li>
+                    ):
+                    (
+                    <Fragment>
                         <li className="nav-item">
                             <NavLink to="/login.html" className="nav-link">Log in</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink to="/sign-up.html" className="nav-link">Sign up</NavLink>
                         </li>
-                    </ul>
-                </nav>
-            </header>
-        );
-    }
-}
+                    </Fragment>
+                    )
+                }
+            </ul>
+        </nav>
+    </header>
+);
+
 export default Header;
