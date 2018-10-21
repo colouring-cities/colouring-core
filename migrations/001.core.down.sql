43f3f4c27e669742fabca51aabc21d027bf7df14
Tom Russell <tomalrussell@gmail.com>
2018-10-21 19:46:14 +0000

    Make sure to drop base tables (and cascade)

--- migrations/001.core.down.sql
+++ migrations/001.core.down.sql
@@ -1,10 +1,10 @@
 -- Drop all tables
-ALTER TABLE buildings DROP COLUMN IF EXISTS revision_id;
-DROP TABLE IF EXISTS logs;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS logs CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS user_sessions;
 DROP TABLE IF EXISTS user_categories;
 DROP TABLE IF EXISTS user_access_levels;
 DROP TABLE IF EXISTS building_properties;
-DROP TABLE IF EXISTS buildings;
-DROP TABLE IF EXISTS geometries;
+DROP TABLE IF EXISTS buildings CASCADE;
+DROP TABLE IF EXISTS geometries CASCADE;
+
