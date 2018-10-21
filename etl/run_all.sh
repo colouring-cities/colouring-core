d72dd903519e154b5ae01a55dcd017ed3029a971
Tom Russell <tomalrussell@gmail.com>
2018-10-21 20:47:31 +0100

    Comment sections in etl run script

--- etl/run_all.sh
+++ etl/run_all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #
-# Filter and transform for loading
+# Extract, transform and load building outlines and property records
 #
 : ${1?"Usage: $0 ./path/to/addressbase/dir ./path/to/mastermap/dir ./path/to/boundary"}
 : ${2?"Usage: $0 ./path/to/addressbase/dir ./path/to/mastermap/dir ./path/to/boundary"}
@@ -12,11 +12,19 @@ mastermap_dir=$2
 boundary_file=$3
 script_dir=${0%/*}
 
+#
+# Process
+#
+
 # extract both datasets
 $script_dir/extract_addressbase.sh $addressbase_dir
 $script_dir/extract_mastermap.sh $mastermap_dir
 # filter mastermap ('building' polygons and any others referenced by addressbase)
 $script_dir/filter_transform_mastermap_for_loading.sh $addressbase_dir $mastermap_dir
+
+#
+# Load
+#
 
 # load all building outlines
 $script_dir/load_geometries.sh $mastermap_dir
