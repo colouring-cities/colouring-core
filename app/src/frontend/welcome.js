ffcd8f17d52af466ea7952e0052706482835380f
Tom Russell <tomalrussell@gmail.com>
2018-11-13 09:27:57 +0000

    Change landing page text
    [Closes #123]

--- app/src/frontend/welcome.js
+++ app/src/frontend/welcome.js
@@ -6,7 +6,21 @@ import './welcome.css';
 const Welcome = () => (
     <div className="jumbotron welcome-float">
         <h1 className="h1">Welcome to Colouring London</h1>
-        <p className="lead">Colour in, view and download data on London's buildings</p>
+
+        <p className="lead">
+        Colouring London is a citizen science platform collecting information on every
+        building in London. We're building it at the Centre for Advanced Spatial Analysis,
+        University College London, to support sustainability research and we're looking for
+        volunteers of all ages and abilities to help.
+        </p>
+        <p className="lead">
+        All you need to do is add information on any London building, to help create beautiful,
+        coloured maps, and a free statistical database for everyone to use. Or just view our
+        maps and test our site before we launch officially next year.
+        </p>
+        <p className="lead">
+        Start colouring buildings and viewing maps now!
+        </p>
         <Link to="/map/age.html"
             className="btn btn-outline-dark btn-lg btn-block">
             Start Colouring
