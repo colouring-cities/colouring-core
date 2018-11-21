298c55038cddfd6a2c57370e39ec1f399bfe3fbe
Tom Russell <tomalrussell@gmail.com>
2018-11-21 22:19:27 +0000

    Update welcome text

--- app/src/frontend/welcome.js
+++ app/src/frontend/welcome.js
@@ -10,20 +10,15 @@ const Welcome = () => (
         <p className="lead">
         Colouring London is a citizen science platform collecting information on every
         building in London. We're building it at the Centre for Advanced Spatial Analysis,
-        University College London, to support sustainability research and we're looking for
-        volunteers of all ages and abilities to help.
+        University College London, to help make London more sustainable.
         </p>
         <p className="lead">
-        All you need to do is add information on any London building, to help create beautiful,
-        coloured maps, and a free statistical database for everyone to use. Or just view our
-        maps and test our site before we launch officially next year.
-        </p>
-        <p className="lead">
-        Start colouring buildings and viewing maps now!
+        Can you help us? We're looking for volunteers of all ages and abilities to test the
+        site before we launch next year. Just add your knowledge to make the buildings colour.
         </p>
         <Link to="/map/age.html"
             className="btn btn-outline-dark btn-lg btn-block">
-            Start Colouring
+            Start Colouring Here!
         </Link>
     </div>
 );
