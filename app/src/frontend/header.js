e6cfb08cb4cfdc3f41e24391499102218cad46d7
Martin-dJ <m.dejode@ucl.ac.uk>
2019-04-10 13:17:58 +0000

    Fix for issue #146 Swap position of 'More about' with 'Building categories' & rename latter

--- app/src/frontend/header.js
+++ app/src/frontend/header.js
@@ -16,19 +16,19 @@ const Header = (props) => (
                     <a className="nav-link" href="https://pages.colouring.london">Hello</a>
                 </li>
                 <li className="nav-item">
+                    <a className="nav-link" href="https://pages.colouring.london/buildingcategories">Data Categories</a>
+                </li>
+                <li className="nav-item">
                     <NavLink to="/view/age.html" className="nav-link">View Maps</NavLink>
                 </li>
                 <li className="nav-item">
                     <NavLink to="/edit/age.html" className="nav-link">Add/Edit Data</NavLink>
                 </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="https://pages.colouring.london/buildingcategories">Building Categories</a>
+                 <li className="nav-item">
+                    <a className="nav-link" href="https://pages.colouring.london/about">More about</a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="https://pages.colouring.london/whoisinvolved">Who's Involved?</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="https://pages.colouring.london/about">More about</a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="https://discuss.colouring.london">Discuss</a>
