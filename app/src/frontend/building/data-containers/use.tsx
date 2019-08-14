f05c1b555289c38b5a0f2785388e2bad88fdfe02
Tom Russell <tomalrussell@gmail.com>
2019-08-14 22:23:12 +0100

    Use view-only notes

--- app/src/frontend/building/data-containers/use.tsx
+++ app/src/frontend/building/data-containers/use.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import withCopyEdit from '../data-container';
-import DataEntry from '../data-components/data-entry';
 
 /**
  * Use view/edit section
  */
 const UseView = (props) => (
-    <dl className="data-list">
-
-    </dl>
+    <Fragment>
+        <p className="data-intro">{props.intro}</p>
+        <ul>
+            <li>Single or multiple use?</li>
+            {
+                // "disabled": true,
+                // "slug": "use_multi",
+                // "type": "checkbox"
+            }
+            <li>Type of use/s</li>
+            {
+                // "disabled": true,
+                // "slug": "use_type",
+                // "type": "text_multi"
+            }
+            <li>Number of self-contained units</li>
+            {
+                // "disabled": true,
+                // "slug": "use_number_scu",
+                // "type": "number",
+                // "step": 1
+            }
+        </ul>
+    </Fragment>
 )
 const UseContainer = withCopyEdit(UseView);
 
