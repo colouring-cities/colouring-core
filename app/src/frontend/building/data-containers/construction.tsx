97ed3f6e1fb044ffb947e5879949cf5214b3b59b
Tom Russell <tomalrussell@gmail.com>
2019-08-14 22:34:47 +0100

    Construction view-only notes

--- app/src/frontend/building/data-containers/construction.tsx
+++ app/src/frontend/building/data-containers/construction.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import withCopyEdit from '../data-container';
 import DataEntry from '../data-components/data-entry';
@@ -7,9 +7,49 @@ import DataEntry from '../data-components/data-entry';
 * Construction view/edit section
 */
 const ConstructionView = (props) => (
-    <dl className="data-list">
-
-    </dl>
+    <Fragment>
+        <p className="data-intro">{props.intro}</p>
+        <ul>
+            <li>Construction system</li>
+            {
+                // "disabled": true,
+                // "slug": "construction_system",
+                // "type": "text"
+            }
+            <li>Primary materials</li>
+            {
+                // "disabled": true,
+                // "slug": "construction_primary_material",
+                // "type": "text"
+            }
+            <li>Secondary materials</li>
+            {
+                // "disabled": true,
+                // "slug": "construction_secondary_material",
+                // "type": "text"
+            }
+            <li>Roofing material</li>
+            {
+                // "disabled": true,
+                // "slug": "construction_roofing_material",
+                // "type": "text"
+            }
+            <li>Percentage of facade glazed</li>
+            {
+                // "disabled": true,
+                // "slug": "construction_facade_percentage_glazed",
+                // "type": "number",
+                // "step": 5
+            }
+            <li>BIM reference or link</li>
+            {
+                // "disabled": true,
+                // "slug": "construction_bim_reference",
+                // "type": "text",
+                // "placeholder": "https://..."
+            }
+        </ul>
+    </Fragment>
 )
 const ConstructionContainer = withCopyEdit(ConstructionView);
 
