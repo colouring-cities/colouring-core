b0df644c2c1b6fe36267433c32132fc970c44ec7
Dominic H <dominijk@gmail.com>
2019-08-29 17:24:53 +0100

    Stashing WIP for #362
    - Complete fields list
    - Completed cat down file
    - Draft cat up file

--- migrations/013.team.down.sql
+++ migrations/013.team.down.sql
@@ -1,12 +1,10 @@
 -- Remove team fields, update in paralell with adding new fields
 
--- Award or awards (may be multiple) stored as json b object 
+--Numeric small int, if > 1 triggers multifield boxes on fron end
+ALTER TABLE buildings DROP COLUMN IF EXISTS number_of_awards;
+
+-- Award or awards (may be multiple) stored as json b object
 ALTER TABLE buildings DROP COLUMN IF EXISTS team_awards;
 
---TODO:  Original building architect
-
---TODO : Original building engineer
-
---TODO : Original building contractor
-
---TODO : Original building developer
+--This is a pair to team_awards each may have one
+ALTER TABLE buildings DROP COLUMN IF EXISTS award_year;
--- migrations/014.team.up.sql
+++ migrations/014.team.up.sql
@@ -1,18 +1,47 @@
 -- Remove team fields, update in paralell with adding new fields
 
--- Award or awards (may be multiple) any of stored as json b object
-ALTER TABLE buildings DROP COLUMN IF EXISTS team_awards;
+
+
+--Numeric small int, if > 1 triggers multifield boxes on fron end
+ALTER TABLE buildings ADD COLUMN IF NOT EXISTS number_of_awards smallint;
+
 
 -- Building award, any of:
--- TBC might be table 
+-- 
 
 CREATE TYPE team_awards
 AS ENUM ('RIBA Gold mdeal',
     'RIBA Sitrling prize',
     'RIBA Regional award',
     'The Civic Trust Award',
-    'Pass',
+    'CIBSE Building Performance Awards',
+    'The Pritzker Architecture Prize',
+    'The Royal Academy of Engineering Major Projects Award',
+    'BREEAM Award',
+    'Civic Voice Design Award',
+    'Stephen Lawrence Prize',
+    'Neave Brown Award',
+    'ICE London Civil Engineering Awards',
+    'Historic England best rescue of a building or place',
+    "Historic England followers' favourite",
+    'New London Architecture awards - Conservation and retrofit',
+    'New London Architecture awards - Mayors Prize',
+    'New London Architecture awards - Culture & Community',
+    'New London Architecture awards - Education',
+    'New London Architecture awards - Homes',
+    'New London Architecture awards - Housing',
+    'New London Architecture awards - Offices',
+    'The Manser Medal - AJ House of the Year',
+    'Architects Jorunal - Heritage Project',
+    'Architects Jorunal - Housing Project',
+    'Architects Jorunal - Community & Faith Project',
+    'Architects Jorunal - Small projects award',
+    'Architects Jorunal - Public Building of the Year',
+    'Architects Jorunal - Refurb of the Year',
     'None');
 
 ALTER TABLE buildings
     ADD COLUMN IF NOT EXISTS team_awards team_awards DEFAULT 'None';
+
+--This is a pair to team_awards each may have one
+ALTER TABLE buildings ADD COLUMN IF NOT EXISTS award_year smallint;
