bc4b4af2d64a75d5e0a98d1effe2392c7ec60421
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2019-10-01 14:57:11 +0100

    Convert all columns to timestamptz in migration

--- migrations/011.bulk-extracts.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS bulk_extracts;
--- migrations/011.bulk-extracts.up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS bulk_extracts (
-    extract_id serial PRIMARY KEY,
-    extracted_on timestamp NOT NULL,
-    extract_path text NOT NULL
-);
