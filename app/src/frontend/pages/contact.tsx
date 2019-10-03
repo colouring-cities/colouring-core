386fb22c10dd7d0ed8a0dadac27f0187c00e2284
Tom Russell <tomalrussell@gmail.com>
2019-10-03 07:49:17 +0100

    Link contact email as mailto

--- app/src/frontend/pages/contact.tsx
+++ app/src/frontend/pages/contact.tsx
@@ -9,13 +9,13 @@ const ContactPage = () => (
             </h1>
             <p> Colouring London has been designed as a sustainable, low-cost model for knowledge exchange/open data platforms able to be reproduced by other towns and cities using our open platform code.</p>
 
-            <p> It is being developed by a small, dedicated team at UCL. We are unable to answer individual queries but welcome constructive comments on how to improve the site, and on other types of data and new features you might like to see.​</p>
+            <p> It is being developed by a small, dedicated team at UCL. We are unable to answer individual queries but welcome constructive comments on how to improve the site, and on other types of data and new features you might like to see.</p>
 
             <p> You can send us comments or ask questions on our discussion threads at <a href="https://discuss.colouring.london/">https://discuss.colouring.london/</a>.</p>
-                    
+
             <p> To view our technical site and platform code please visit Github at: <a href="https://github.com/tomalrussell/colouring-london">https://github.com/tomalrussell/colouring-london</a>.</p>
 
-            <p>For press enquiries please contact the Bartlett Press and Communications team at architecture@ucl.ac.uk.</p>
+            <p>For press enquiries please contact the Bartlett Press and Communications team at <a href="mailto:architecture@ucl.ac.uk">architecture@ucl.ac.uk</a></p>
 
             <p>
                 <img className="d-block mx-auto" src="images/logo-cl.png"></img>
