5d3eff480075b391698e9d6614e44a4b2ba153c7
mz8i <maciej.ziarkowski@gmail.com>
2019-10-28 16:46:22 +0000

    Branding adjustments (#471)
    * Synchronise category colours with WIX page version
    
    * Display static colourful logo on map pages

--- app/src/frontend/components/logo.tsx
+++ app/src/frontend/components/logo.tsx
@@ -26,22 +26,22 @@ const Logo: React.FunctionComponent<LogoProps> = (props) => {
 const LogoGrid: React.FunctionComponent = () => (
     <div className="grid">
         <div className="row">
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
+            <div className="cell background-location"></div>
+            <div className="cell background-use"></div>
+            <div className="cell background-type"></div>
+            <div className="cell background-age"></div>
         </div>
         <div className="row">
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
+            <div className="cell background-size"></div>
+            <div className="cell background-construction"></div>
+            <div className="cell background-streetscape"></div>
+            <div className="cell background-team"></div>
         </div>
         <div className="row">
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
+            <div className="cell background-sustainability"></div>
+            <div className="cell background-community"></div>
+            <div className="cell background-planning"></div>
+            <div className="cell background-like"></div>
         </div>
     </div>
 )
