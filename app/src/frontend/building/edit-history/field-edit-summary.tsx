ffcf31cb346165ea65f674862420abff3ce8a38c
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2019-11-06 15:39:44 +0000

    Format boolean values in edit history

--- app/src/frontend/building/edit-history/field-edit-summary.tsx
+++ app/src/frontend/building/edit-history/field-edit-summary.tsx
@@ -6,12 +6,19 @@ interface FieldEditSummaryProps {
     oldValue: any;
 }
 
+function formatValue(value: any) {
+    if(typeof value === 'boolean') {
+        return value ? 'Yes' : 'No';
+    }
+    return value;
+}
+
 const FieldEditSummary: React.FunctionComponent<FieldEditSummaryProps> = props => (
     <>
         {props.title}:&nbsp;
-        <code title="Value before edit" className='edit-history-diff old'>{props.oldValue}</code>
+        <code title="Value before edit" className='edit-history-diff old'>{formatValue(props.oldValue)}</code>
         &nbsp;
-        <code title="Value after edit" className='edit-history-diff new'>{props.value}</code>
+        <code title="Value after edit" className='edit-history-diff new'>{formatValue(props.value)}</code>
     </>
 );
 
