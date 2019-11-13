70a24f6367e207318ba880642ca6436848d3596c
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2019-11-13 19:10:02 +0000

    Adjust welcome text and link color

--- app/src/frontend/pages/welcome.tsx
+++ app/src/frontend/pages/welcome.tsx
@@ -10,20 +10,13 @@ const Welcome = () => (
         <p className="lead">
         Colouring London is a knowledge exchange platform collecting information on every
         building in London, to help make the city more sustainable. We&rsquo;re building it at The
-        Bartlett Centre for Advanced Spatial Analysis, University College London.
-        </p>
-        <p className="lead">
-        Can you help us? We&rsquo;re still at an early stage of development, and we&rsquo;re looking for
-        volunteers of all ages and abilities to test and provide feedback on the site as we
-        build it.
-        </p>
-        <p className="lead">
-        All of the data we collect is made <Link to="/data-extracts.html">openly available</Link> &ndash;
-        please read our <a href="https://www.pages.colouring.london/data-ethics">data ethics policy</a> and
-        credit Colouring London if you use or share our maps or data.
+        Bartlett Centre for Advanced Spatial Analysis, University College London. We're still at an early stage of development. Please help us by providing feedback on the site as we create it.
         </p>
         <p className="lead">
             We collect our building data from many sources. Though we are unable to vouch for their accuracy, we are currently experimenting with a range of features including 'data source', 'edit history', and 'entry verification', to assist you in checking reliability and judging how suitable the data are for your intended use.
+        </p>
+        <p className="lead">
+        All of the data we collect is made <Link to="/data-extracts.html">openly available</Link>. We just ask you to credit Colouring London and read our <a href="https://www.pages.colouring.london/data-ethics">data ethics policy</a>  when using or sharing our data and maps.
         </p>
         <Link to="/view/categories"
             className="btn btn-outline-dark btn-lg btn-block">
