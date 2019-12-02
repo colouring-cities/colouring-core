7b8215517ac2654e5a2bf2f84c016e99a5b53855
Dominic H <dominijk@gmail.com>
2019-12-02 15:57:22 +0000

    Update to migrations for staging test

--- migrations/0xx.landuse.down.sql
+++ migrations/0xx.landuse.down.sql
@@ -1,9 +1,9 @@
 --Landuse is hierachical. Highest level is Order (ie. Residential) then Group (ie Residential-Dwelling) then Class (ie Residential-Dwelling-Detached house)
 --Interface will collected most detailed (class) but visualise highest level (order)
 --Landuse is a table as #358
-
--- Land use is table with 3 levels of hierachy (highest to lowest). order > group > class
-DROP TABLE IF EXISTS landuse_classifications;
+--Land use class, group and order will be stored in a new table
+DROP TABLE IF EXISTS reference_tables.buildings_landuse_order CASCADE;
+DROP TABLE IF EXISTS reference_tables.buildings_landuse_class CASCADE;
 
 -- Land use class or classes, array object, client constrained.
 ALTER TABLE buildings DROP COLUMN IF EXISTS current_landuse_class;
@@ -11,8 +11,7 @@ ALTER TABLE buildings DROP COLUMN IF EXISTS current_landuse_class;
 -- Land use order, singular. Client and db constrained.
 ALTER TABLE buildings DROP COLUMN IF EXISTS current_landuse_order;
 
---Land use class, group and order will be stored in a new table
-DROP TABLE IF EXISTS building_landuse CASCADE;
+
 
 --===========================================
 --
