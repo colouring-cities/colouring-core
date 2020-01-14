d2a1469b722004812d6adc8f1edfcd8ae6611fe8
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2020-01-14 22:53:22 +0000

    Separate out land use data access layer

---
+++ app/src/api/dataAccess/landUse.ts
@@ -0,0 +1,38 @@
+import db from '../../db';
+
+export async function getLanduseGroupFromClass(classes: string[]): Promise<string[]> {
+    if (classes.length === 0) return [];
+
+    return (await db.many(
+        `
+        SELECT DISTINCT parent.description
+        FROM reference_tables.buildings_landuse_group AS parent
+        JOIN reference_tables.buildings_landuse_class AS child
+        ON child.parent_group_id = parent.landuse_id
+        WHERE child.description IN ($1:csv)
+        ORDER BY parent.description`,
+        [classes]
+    )).map(x => x.description);
+}
+
+export async function getLandUseOrderFromGroup(groups: string[]): Promise<string> {
+    if(groups.length === 0) return null;
+
+    const orders = (await db.many(
+        `
+        SELECT DISTINCT parent.description
+        FROM reference_tables.buildings_landuse_order AS parent
+        JOIN reference_tables.buildings_landuse_group AS child
+        ON child.parent_order_id = parent.landuse_id
+        WHERE child.description IN ($1:csv)
+        ORDER BY parent.description
+        `,
+        [groups]
+    )).map(x => x.description);
+
+    if(orders.length === 1) {
+        return orders[0];
+    } else if (orders.length > 1) {
+        return 'Mixed Use';
+    } else return null;
+}
--- app/src/api/services/domainLogic/processBuildingUpdate.ts
+++ app/src/api/services/domainLogic/processBuildingUpdate.ts
@@ -2,10 +2,10 @@ import { hasAnyOwnProperty } from '../../../helpers';
 
 import { processCurrentLandUseClassifications } from './currentLandUseClassifications';
 
-export async function processBuildingUpdate(buildingId: number, building: any): Promise<any> {
-    if(hasAnyOwnProperty(building, ['current_landuse_class', 'current_landuse_group', 'current_landuse_order'])) {
-        building = await processCurrentLandUseClassifications(buildingId, building);
+export async function processBuildingUpdate(buildingId: number, buildingUpdate: any): Promise<any> {
+    if(hasAnyOwnProperty(buildingUpdate, ['current_landuse_class', 'current_landuse_group', 'current_landuse_order'])) {
+        buildingUpdate = await processCurrentLandUseClassifications(buildingId, buildingUpdate);
     }
 
-    return building;
+    return buildingUpdate;
 }
