88d1032e13f3cd55fde07d489e210cd1da7f0154
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2020-01-23 10:31:20 +0000

    Rename field to revision_timestamp in edit history

--- app/src/api/services/editHistory.ts
+++ app/src/api/services/editHistory.ts
@@ -3,7 +3,7 @@ import db from '../../db';
 async function getGlobalEditHistory() {
     try {
         return await db.manyOrNone(
-            `SELECT log_id as revision_id, forward_patch, reverse_patch, date_trunc('minute', log_timestamp), username, building_id
+            `SELECT log_id as revision_id, forward_patch, reverse_patch, date_trunc('minute', log_timestamp) as revision_timestamp, username, building_id
             FROM logs, users
             WHERE logs.user_id = users.user_id
                 AND log_timestamp >= now() - interval '7 days'
