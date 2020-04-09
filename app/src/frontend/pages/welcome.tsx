a2393a019a0a7f1a3564deb174f96903b2b5aba9
Tom Russell <tomalrussell@gmail.com>
2020-04-09 16:10:07 +0100

    Tweak welcome text

--- app/src/frontend/pages/welcome.tsx
+++ app/src/frontend/pages/welcome.tsx
@@ -8,26 +8,30 @@ const Welcome = () => (
         <h1 className="h2">Welcome to Colouring London!</h1>
         <p>
 
+            We collect and provide open data about buildings.
+
+        </p>
+        <p>
+
             Colouring London is a knowledge exchange platform set up by University College
-            London to help make the city more sustainable. It provides open statistical data
-            on the characteristics of the city's buildings and on the dynamic behaviour of the
-            stock. We're working to collate, collect, generate, verify over fifty types of
-            data and to visualise many of these datasets.
+            London. It provides open statistical data about the city's buildings and the
+            dynamic behaviour of the stock. We're working to collate, collect, generate,
+            verify over fifty types of data and to visualise many of these datasets.
 
         </p>
         <p>
 
             Our information comes from many different sources. As we are unable to vouch for
-            data accuracy, we are currently experimenting with a range of features including
-            'data source', 'edit history', and 'entry verification', to assist you in checking
-            reliability and judging how suitable the data are for your intended use. Your help
+            data accuracy, we are experimenting with how to present data sources, how data
+            are edited over time, and how to ask for data verification, to help you to check
+            reliability and judge how suitable the data are for your intended use. Your help
             in checking and adding data is very much appreciated.
 
         </p>
         <p>
 
             All data we collect are made <Link to="/data-extracts.html">openly
-            available</Link>. We just ask you to credit Colouring London and read our <a
+            available</Link>. We ask you to credit Colouring London and read our <a
             href="https://www.pages.colouring.london/data-ethics">data ethics policy</a> when
             using or sharing our data, maps or <a
             href="https://github.com/colouring-london/colouring-london">code</a>.
