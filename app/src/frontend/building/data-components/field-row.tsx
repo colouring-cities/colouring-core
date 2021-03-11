8a42f93dd2f37ab97bf4cdf406fb3e9b76d487b2
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2021-03-11 19:19:20 +0000

    Add a field row component

---
+++ app/src/frontend/building/data-components/field-row.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './field-row.css';
+
+export function FieldRow({children}) {
+    return (
+        <div className="field-row">
+            {children}
+        </div>
+    )
+}
