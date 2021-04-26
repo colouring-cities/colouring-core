ac3a9ad45a1861492221bdfbd328f373dd97a475
Maciej Ziarkowski <maciej.ziarkowski@gmail.com>
2021-04-26 23:53:15 +0100

    Update/install explicitly lodash

--- app/src/api/services/__tests__/domainLogic/landUse.test.ts
+++ app/src/api/services/__tests__/domainLogic/landUse.test.ts
@@ -1,4 +1,4 @@
-import * as _ from 'lodash';
+import _ from 'lodash';
 
 import { ArgumentError } from '../../../errors/general';
 import { LandUseState, updateLandUse } from '../../domainLogic/landUse';
