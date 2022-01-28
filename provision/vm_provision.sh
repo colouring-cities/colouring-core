3a5a4a0ed1e844d0f83c878f783b52afa9f79464
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-01-28 09:28:32 +0000

    point apt to the postgres repository

--- provision/vm_provision.sh
+++ provision/vm_provision.sh
@@ -17,6 +17,11 @@ set -x
 # Create vagrant user if not exists
 id -u vagrant >/dev/null 2>&1 || useradd --create-home vagrant
 
+# Create the file repository configuration:
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+# Import the repository signing key:
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 
 #
 # Install OS packages
