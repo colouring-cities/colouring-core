fe94a51da4856e312bb937b221592247d12570bc
Mateusz Konieczny <matkoniecz@gmail.com>
2022-01-31 23:07:35 +0100

    Modify already commited migration
    this avoids confusing migration in repository that could damage data, as meaning of the data in database mismatched decription

--- migrations/025.actual.sql
+++ migrations/025.actual.sql
@@ -1,30 +1,33 @@
--- equivalent to 015 migration
 INSERT INTO reference_tables.landuse_classifications
 (landuse_id, description, level, parent_id, is_used)
 VALUES
--- order
 ('U15X','Unclassified, presumed non-residential','order',NULL,True);
-
-
--- equivalent to 016 migration
 INSERT INTO reference_tables.buildings_landuse_order
 (landuse_id, description)
 VALUES
--- order
 ('U15X','Unclassified, presumed non-residential');
 
-
--- equivalent to 015 migration
 INSERT INTO reference_tables.landuse_classifications
 (landuse_id, description, level, parent_id, is_used)
 VALUES
--- group
 ('U073X','Unclassified, presumed non-residential','group','U15X',True);
-
-
--- equivalent to 016 migration
 INSERT INTO reference_tables.buildings_landuse_group
 (landuse_id, description, parent_order_id)
 VALUES
--- group
-('U073X','Unclassified, presumed non-residential','U15X');
+('U073X','Unclassified, presumed non-residential', 'U15X');
+
+UPDATE buildings
+SET current_landuse_order = 'Unclassified, presumed non-residential'
+WHERE current_landuse_order = 'Unclassified buildings';
+
+DELETE FROM reference_tables.landuse_classifications
+WHERE landuse_id='U14X';
+
+DELETE FROM reference_tables.buildings_landuse_order
+WHERE landuse_id='U14X';
+
+DELETE FROM reference_tables.landuse_classifications
+WHERE landuse_id='U072X';
+
+DELETE FROM reference_tables.buildings_landuse_group
+WHERE landuse_id='U072X';
