b4475f69384ada01d7d54f388c86942de650ca7f
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-02-11 16:07:07 +0000

    just use installed python3 instead of virtual environment

--- init-user-db.sh
+++ init-user-db.sh
@@ -8,13 +8,6 @@ apt-get install -y gdal-bin libspatialindex-dev libgeos-dev libproj-dev
 
 apt-get install -y python3 python3-pip python3-dev
 
-# Install pyenv (for some reason installing with apt-get didn't work in docker)
-apt-get install -y curl git
-echo export PATH="~/.pyenv/bin:$PATH" >> ~/.bashrc
-eval "$(pyenv init -)" >> ~/.bashrc
-eval "$(pyenv virtualenv-init -)" >> ~/.bashrc
-source ~/.bashrc
-
 psql -d colouringlondon -U dockeruser -c "SELECT 1 FROM pg_user WHERE usename = 'dockeruser';" | grep -q 1 ||  psql -d colouringlondon -U dockeruser -c "CREATE ROLE dockeruser SUPERUSER LOGIN PASSWORD 'postgres';"
 psql -d colouringlondon -U dockeruser -c "SELECT 1 FROM pg_database WHERE datname = 'colouringlondon';" | grep -q 1 ||  -u postgres createdb -E UTF8 -T template0 --locale=en_US.utf8 -O dockeruser colouringlondon
 
@@ -24,8 +17,6 @@ psql -d colouringlondon -U dockeruser -c "create extension pg_trgm;"
 
 ls ./colouring-london/migrations/*.up.sql 2>/dev/null | while read -r migration; do psql -d colouringlondon < $migration; done;
 
-pyvenv colouringlondon
-source colouringlondon/bin/activate
 pip install --upgrade pip
 pip install --upgrade setuptools wheel
 pip install -r ./colouring-london/etl/requirements.txt
