2fe85a792330f135c24757fac3bf83ed2b944dd9
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-02-14 09:44:06 +0000

    first attempt docker

---
+++ etl/load_geometries_cl.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#
+# Load geometries from GeoJSON to Postgres
+# - assume postgres connection details are set in the environment using PGUSER, PGHOST etc.
+#
+: ${1?"Usage: $0 ./path/to/mastermap/dir"}
+
+mastermap_dir=$1
+
+#
+# Create 'geometry' record with
+#     id: <polygon-guid>,
+#     source_id: <toid>,
+#     geom: <geom>
+#
+find $mastermap_dir -type f -name '*.3857.csv' \
+-printf "$mastermap_dir/%f\n" | \
+parallel \
+cat {} '|' psql -d colouringlondon -c "\"COPY geometries ( geometry_geom, source_id ) FROM stdin WITH CSV HEADER;\""
+
+#
+# Delete any duplicated geometries (by TOID)
+#
+psql -d colouringlondon -c "DELETE FROM geometries a USING (
+    SELECT MIN(ctid) as ctid, source_id
+    FROM geometries
+    GROUP BY source_id
+    HAVING COUNT(*) > 1
+) b
+WHERE a.source_id = b.source_id
+AND a.ctid <> b.ctid;"
