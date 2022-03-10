5b06b12f9852af7b87b68ad9c271be8d7aa5c59d
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-03-10 13:51:05 +0000

    temp hard code python path

--- etl/filter_transform_mastermap_for_loading.sh
+++ etl/filter_transform_mastermap_for_loading.sh
@@ -9,19 +9,17 @@
 addressbase_dir=$1
 mastermap_dir=$2
 
-PATH+=:~/colouring-london/etl/colouringlondon/bin/python
-
 #
 # Check which TOIDs are matched against UPRNs
 #
-python check_ab_mm_match.py $addressbase_dir $mastermap_dir
+colouringlondon/bin/python check_ab_mm_match.py $addressbase_dir $mastermap_dir
 
 #
 # Filter
 # - WHERE descriptiveGroup = '(1:Building)'
 # - OR toid in addressbase_toids
 #
-python filter_mastermap.py $addressbase_dir $mastermap_dir
+colouringlondon/bin/python filter_mastermap.py $addressbase_dir $mastermap_dir
 
 #
 # Transform to 3857 (web mercator)
