103300f969fac2fef1dc6dd02d1cd25bfef28360
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-03-18 13:36:06 +0000

    remove not needed non-buildings toid code

--- etl/filter_mastermap.py
+++ etl/filter_mastermap.py
@@ -8,7 +8,6 @@ import json
 import os
 import sys
 
-from multiprocessing import Pool
 
 csv.field_size_limit(sys.maxsize)
 
@@ -20,19 +19,18 @@ def main(mastermap_path):
 
 def filter(mm_path)
     output_path =  "{}.filtered.csv".format(str(mm_path).replace(".gml.csv", ""))
-    alt_output_path =  "{}.filtered_not_building.csv".format(str(mm_path).replace(".gml.csv", ""))
     output_fieldnames = ('WKT', 'fid', 'descriptiveGroup')
+    # Open the input csv with all polygons, buildings and others
     with open(mm_path, 'r') as fh:
         r = csv.DictReader(fh)
+        # Open a new buildings csv
         with open(output_path, 'w') as output_fh:
             w = csv.DictWriter(output_fh, fieldnames=output_fieldnames)
-            w.writeheader()
-            with open(alt_output_path, 'w') as alt_output_fh:
-                alt_w = csv.DictWriter(alt_output_fh, fieldnames=output_fieldnames)
-                alt_w.writeheader()
-                for line in r:
-                    if 'Building' in line['descriptiveGroup']:
-                        w.writerow(line)
+            w.writeheader()    
+            # Then write to the output csv buildings only
+            for line in r:
+                if 'Building' in line['descriptiveGroup']:
+                    w.writerow(line)
 
 
 if __name__ == '__main__':
