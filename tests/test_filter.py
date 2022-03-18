158a09637b70813119bee3d7ddc896ca1aaeae40
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-03-18 15:30:17 +0000

    add failing test for missing mastermap type

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -13,10 +13,11 @@ def test_filter_mastermap():
     assert len(csv_array) == 2  # assert that length is 2 because just one row after header
     
     
-# def test_filter_mastermap_missing_type():
-#     """Test that MasterMap CSV can be correctly filtered when the polygon does not have a type specified."""
-#     input_file = "tests/test_mastermap_missing_type.gml.csv"  # Test csv with one building and one non-building
-#     filter_mastermap(input_file)  # creates test_mastermap.filtered.csv
-#     with open('tests/test_mastermap.filtered.csv', newline='') as csvfile:
-#         csv_array = list(csv.reader(csvfile))
-#     assert len(csv_array) == 2  # assert that length is 2 because just one row after header
+def test_filter_mastermap_missing_type():
+    """Test that MasterMap CSV can be correctly filtered when the polygon does not have a type specified."""
+    input_file = "tests/test_mastermap_missing_type.gml.csv"  # Test csv with one building and one non-building
+    output_file = input_file.replace('gml', 'filtered')
+    filter_mastermap(input_file)  # creates test_mastermap.filtered.csv
+    with open(output_file, newline='') as csvfile:
+        csv_array = list(csv.reader(csvfile))
+    assert len(csv_array) == 2  # assert that length is 2 because just one row after header
