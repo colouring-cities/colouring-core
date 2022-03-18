35f85ee2edbee20c5b1e81420f118e5fe5e81328
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-03-18 15:04:09 +0000

    test mastermap filtering works

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,8 +1,12 @@
+import csv
 import pytest
 from etl import filter_mastermap
 
+
 def test_filter_mastermap():
     """Test that MasterMap CSV can be correctly filtered to include only buildings."""
-    input_file = ""
-    expected_output = ""
-    assert filter_mastermap(input_file) == expected_output
+    input_file = "tests/test_mastermap.gml.csv"  # Test csv with one building and one non-building
+    filter_mastermap(input_file)  # creates test_mastermap.filtered.csv
+    with open('tests/test_mastermap.filtered.csv', newline='') as csvfile:
+        csv_array = list(csv.reader(csvfile))
+    assert len(csv_array) == 2  # assert that length is 2 because just one row after header
