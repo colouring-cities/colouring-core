565bd68d40e83bcfb7ef7e9109aee2be36b25518
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-03-24 15:01:11 +0000

    dont create  db - should exist

--- init-user-db.sh
+++ init-user-db.sh
@@ -8,8 +8,9 @@ apt-get install -y gdal-bin libspatialindex-dev libgeos-dev libproj-dev
 
 apt-get install -y python3 python3-pip python3-dev
 
-psql -d colouringlondon -U dockeruser -c "SELECT 1 FROM pg_user WHERE usename = 'dockeruser';" | grep -q 1 ||  psql -d colouringlondon -U dockeruser -c "CREATE ROLE dockeruser SUPERUSER LOGIN PASSWORD 'postgres';"
-psql -d colouringlondon -U dockeruser -c "SELECT 1 FROM pg_database WHERE datname = 'colouringlondon';" | grep -q 1 ||  -u postgres createdb -E UTF8 -T template0 --locale=en_US.utf8 -O dockeruser colouringlondon
+# Do I need to create a db? Look at the dockerhub
+# psql -d colouringlondon -U dockeruser -c "SELECT 1 FROM pg_user WHERE usename = 'dockeruser';" | grep -q 1 ||  psql -d colouringlondon -U dockeruser -c "CREATE ROLE dockeruser SUPERUSER LOGIN PASSWORD 'postgres';"
+# psql -d colouringlondon -U dockeruser -c "SELECT 1 FROM pg_database WHERE datname = 'colouringlondon';" | grep -q 1 ||  -u postgres createdb -E UTF8 -T template0 --locale=en_US.utf8 -O dockeruser colouringlondon
 
 psql -d colouringlondon -U dockeruser -c "create extension postgis;"
 psql -d colouringlondon -U dockeruser -c "create extension pgcrypto;"
