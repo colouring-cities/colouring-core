7297354f5baabc5e25eb3afbb562cf3dddc5bf30
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-03-29 10:12:31 +0100

    update test that so that it fails

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -5,12 +5,12 @@ from etl import filter_mastermap
 
 def test_filter_mastermap():
     """Test that MasterMap CSV can be correctly filtered to include only buildings."""
-    input_file = "tests/test_mastermap.gml.csv"  # Test csv with one building and one non-building
+    input_file = "tests/test_mastermap.gml.csv"  # Test csv with two buildings and one non-building
     output_file = input_file.replace('gml', 'filtered')
     filter_mastermap(input_file)  # creates output_file
     with open(output_file, newline='') as csvfile:
         csv_array = list(csv.reader(csvfile))
-    assert len(csv_array) == 2  # assert that length is 2 because just one row after header
+    assert len(csv_array) == 3  # assert that length is 3 because just two building rows after header
     
     
 def test_filter_mastermap_missing_descriptivegroup():
