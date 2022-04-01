aab780e07f03af4a360b03755ef834716fdd389d
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-04-01 14:56:58 +0100

    add missing column

--- etl/load_coordinates.sh
+++ etl/load_coordinates.sh
@@ -23,13 +23,15 @@ echo "Loading Open TOID CSV(s) to temporary table..."
 find $opentoid_dir -type f -name '*.csv' \
 -printf "$opentoid_dir/%f\n" | \
 parallel \
-cat {} '|' psql -c "\"COPY open_toid ( toid, version_number, version_date, easting, northing ) FROM stdin WITH CSV HEADER;\""
+cat {} '|' psql -c "\"COPY open_toid ( toid, version_number, version_date, source_product, easting, northing ) FROM stdin WITH CSV HEADER;\""
 
 # Convert the northing/easting coordinates to latitude/longitute with PostGIS
 psql -c "update open_toid set the_geom=GeomFromText('POINT('||easting||' '||northing||')',27700);"
 psql -c "update open_toid set longitude=st_x(st_transform(the_geom,4326)), latitude=st_y(st_transform(the_geom,4326));"
 
 # Update the buildings table with coordinates
+# psql -c ""
 
 
 # Delete the temporary table
+# psql -c "DROP TABLE open_toid;"
