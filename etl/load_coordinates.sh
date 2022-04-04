ec948e15cbfe9b194de580f1873c38635d2cb2a0
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-04-04 11:11:31 +0100

    separate python step since memory failiure is possible

--- etl/load_coordinates.sh
+++ etl/load_coordinates.sh
@@ -7,9 +7,6 @@
 
 opentoid_dir=$1
 
-echo "Converting OSGB36 Eastings and Northings to WGS84 longitude and latitude coordinates..."
-python convert_opentoid_bng_latlon.py $opentoid_dir
-
 echo "Creating a temporary table for coordinates..."
 psql -c "CREATE TABLE open_toid (
     toid varchar,
@@ -28,14 +25,8 @@ find $opentoid_dir -type f -name '*.csv' \
 parallel \
 cat {} '|' psql -c "\"COPY open_toid ( toid, version_number, version_date, source_product, easting, northing, longitude, latitute ) FROM stdin WITH CSV HEADER;\""
 
-# Convert the northing/easting coordinates to latitude/longitute with PostGIS
-# psql -c "select AddGeometryColumn( 'public', 'open_toid', 'the_geom', 27700, 'POINT', 2);"
-# psql -c "update open_toid set the_geom=GeomFromText('POINT('||easting||' '||northing||')',27700);"
-# psql -c "update open_toid set longitude=st_x(st_transform(the_geom,4326)), latitude=st_y(st_transform(the_geom,4326));"
-
 # Update the buildings table with coordinates
 # psql -c ""
-
 
 # Delete the temporary table
 # psql -c "DROP TABLE open_toid;"
