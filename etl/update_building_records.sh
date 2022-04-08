f4a85e7a68a4fa23cfd7ab16dc2d2e64f0d95afd
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-04-08 15:15:39 +0100

    comment

--- etl/update_building_records.sh
+++ etl/update_building_records.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+## Comments:
+## Is dynamics_has_demolished_buildings = TRUE sufficient to prevent buildings being loaded? - No
+## Lets mark them, then see if there even are any
+## Describe these changes in PR and ask for comment if the bool is sufficient
+
 ## Prerequisites:
 ## - Make geometries table have coordinates (load_coordinates.sh) - DONE
 ## - <mastermap download>= release_geometries
@@ -19,6 +24,3 @@
   # secondarily, if building.coordinates <10m away from any new_geometry.coordinates
     # older_building.dynamics_has_demolished_buildings = TRUE
     # link new_geometry TOID in the geometries table to old building and delete duplicate building we just created for new_geometry
-
-
-# Worry about build dates for switching next
