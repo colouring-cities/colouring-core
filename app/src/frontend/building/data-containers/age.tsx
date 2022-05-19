252d7ba214e1b339c72ac8b56e0dcd151429da05
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-05-19 10:31:48 +0100

    remove commented

--- app/src/frontend/building/data-containers/age.tsx
+++ app/src/frontend/building/data-containers/age.tsx
@@ -37,25 +37,15 @@ const AgeView: React.FunctionComponent<CategoryViewProps> = (props) => {
                   verified_count={props.building.verified.date_year}
                   
                   allow_verify={props.user !== undefined && props.building.date_upper !== null && !props.edited}
-                  // onVerify={props.onVerify}
                   user_verified={props.user_verified.hasOwnProperty("date_upper")}
                   user_verified_as={props.user_verified.date_upper}
                   verified_count={props.building.verified.date_upper}
                   
                   allow_verify={props.user !== undefined && props.building.date_lower !== null && !props.edited}
-                  // onVerify={props.onVerify}
                   user_verified={props.user_verified.hasOwnProperty("date_lower")}
                   user_verified_as={props.user_verified.date_lower}
                   verified_count={props.building.verified.date_lower}
                   />
-              // <Verification
-              //     slug="date_year"
-              //     allow_verify={props.user !== undefined && props.building.date_year !== null && !props.edited}
-              //     onVerify={props.onVerify}
-              //     user_verified={props.user_verified.hasOwnProperty("date_year")}
-              //     user_verified_as={props.user_verified.date_year}
-              //     verified_count={props.building.verified.date_year}
-              //     />
               <NumericDataEntry
                   title={dataFields.facade_year.title}
                   slug="facade_year"
@@ -116,25 +106,15 @@ const AgeView: React.FunctionComponent<CategoryViewProps> = (props) => {
                 verified_count={props.building.verified.date_year}
                 
                 allow_verify={props.user !== undefined && props.building.date_upper !== null && !props.edited}
-                // onVerify={props.onVerify}
                 user_verified={props.user_verified.hasOwnProperty("date_upper")}
                 user_verified_as={props.user_verified.date_upper}
                 verified_count={props.building.verified.date_upper}
                 
                 allow_verify={props.user !== undefined && props.building.date_lower !== null && !props.edited}
-                // onVerify={props.onVerify}
                 user_verified={props.user_verified.hasOwnProperty("date_lower")}
                 user_verified_as={props.user_verified.date_lower}
                 verified_count={props.building.verified.date_lower}
                 />
-            // <Verification
-            //     slug="date_year"
-            //     allow_verify={props.user !== undefined && props.building.date_year !== null && !props.edited}
-            //     onVerify={props.onVerify}
-            //     user_verified={props.user_verified.hasOwnProperty("date_year")}
-            //     user_verified_as={props.user_verified.date_year}
-            //     verified_count={props.building.verified.date_year}
-            //     />
             <NumericDataEntry
                 title={dataFields.facade_year.title}
                 slug="facade_year"
