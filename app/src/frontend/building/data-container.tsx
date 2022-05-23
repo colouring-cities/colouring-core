1763af155e15513127ea2982d37fdf4441d6b9fb
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-05-23 11:34:15 +0100

    fake_slug hack

--- app/src/frontend/building/data-container.tsx
+++ app/src/frontend/building/data-container.tsx
@@ -248,7 +248,7 @@ const withCopyEdit: (wc: React.ComponentType<CategoryViewProps>) => DataContaine
             }
             
             const edits = (slug == 'current_landuse_group') ?
-             {['date_year']: value} : 
+             {['fake_slug']: value} : 
              {[slug]: value}
 
             this.doSubmit(edits);
