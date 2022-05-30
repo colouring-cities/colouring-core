e536ca4ef05c4cfba12017ae7bb2f7a69efb23d0
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-05-30 14:30:36 +0100

    refactor increment to use props

--- app/src/frontend/building/data-components/verification.tsx
+++ app/src/frontend/building/data-components/verification.tsx
@@ -21,9 +21,9 @@ class Verification extends Component<VerificationProps, any> {
 
     handleClick(verify) {
         if (verify) {
-            this.setState({ verified_count: this.verified_count + 1 })
+            this.setState({ props.verified_count: this.props.verified_count + 1 })
         } else {
-            this.setState({ verified_count: this.verified_count - 1 })
+            this.setState({ props.verified_count: this.props.verified_count - 1 })
         }
         return (e) => {
             e.preventDefault();
