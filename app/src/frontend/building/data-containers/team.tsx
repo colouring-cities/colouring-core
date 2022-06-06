7aa7f81ffe8ffef3aa422b2cefb1678232de846e
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-06-06 10:31:43 +0100

    Revert "2nd container test"
    This reverts commit a2d1ec41cf7f032a0d61324afb1ade464fc91f48.

--- app/src/frontend/building/data-containers/team.tsx
+++ app/src/frontend/building/data-containers/team.tsx
@@ -114,7 +114,5 @@ const TeamView: React.FunctionComponent<CategoryViewProps> = (props) => {
     );
 };
 const TeamContainer = withCopyEdit(TeamView);
-const TeamContainer2 = withCopyEdit(TeamView);
 
 export default TeamContainer;
-export default TeamContainer2;
