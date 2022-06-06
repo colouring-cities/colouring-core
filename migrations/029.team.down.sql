db9159e24c7f492fd91907d63313f72fc41e0900
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-06-06 11:48:39 +0100

    add team migrations

---
+++ migrations/029.team.down.sql
@@ -0,0 +1,8 @@
+ALTER TABLE buildings DROP COLUMN IF EXISTS extension_year;
+ALTER TABLE buildings DROP COLUMN IF EXISTS developer_type;
+ALTER TABLE buildings DROP COLUMN IF EXISTS designers;
+ALTER TABLE buildings DROP COLUMN IF EXISTS designers_source_link;
+ALTER TABLE buildings DROP COLUMN IF EXISTS designers;
+ALTER TABLE buildings DROP COLUMN IF EXISTS lead_designer_type;
+ALTER TABLE buildings DROP COLUMN IF EXISTS designer_awards;
+ALTER TABLE buildings DROP COLUMN IF EXISTS awards_source_link;
---
+++ migrations/029.team.up.sql
@@ -0,0 +1,8 @@
+ALTER TABLE buildings ADD COLUMN IF NOT EXISTS extension_year smallint;
+ALTER TABLE buildings ADD COLUMN IF NOT EXISTS developer_type varchar;
+ALTER TABLE buildings ADD COLUMN IF NOT EXISTS designers varchar;
+ALTER TABLE buildings ADD COLUMN IF NOT EXISTS designers_source_link text[];
+ALTER TABLE buildings ADD COLUMN IF NOT EXISTS designers varchar;
+ALTER TABLE buildings ADD COLUMN IF NOT EXISTS lead_designer_type varchar;
+ALTER TABLE buildings ADD COLUMN IF NOT EXISTS designer_awards boolean null;
+ALTER TABLE buildings ADD COLUMN IF NOT EXISTS awards_source_link text[];
