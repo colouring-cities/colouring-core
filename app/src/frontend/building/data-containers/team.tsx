68abacc685ac2bb48d001b886c73a7c001663f78
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-06-10 13:32:46 +0100

    add developer_name

--- app/src/frontend/building/data-containers/team.tsx
+++ app/src/frontend/building/data-containers/team.tsx
@@ -91,6 +91,23 @@ const TeamView: React.FunctionComponent<CategoryViewProps> = (props) => {
               user_verified_as={props.user_verified.developer_type}
               verified_count={props.building.verified.developer_type}
               />
+          <SelectDataEntry
+              slug='developer_name'
+              title={dataFields.developer_name.title}
+              value={props.building.developer_name}
+              options={dataFields.developer_name.items}
+              onChange={props.onChange}
+              mode={props.mode}
+              copy={props.copy}
+          />
+          <Verification
+              slug="developer_name"
+              allow_verify={props.user !== undefined && props.building.developer_name !== null && !props.edited}
+              onVerify={props.onVerify}
+              user_verified={props.user_verified.hasOwnProperty("developer_name")}
+              user_verified_as={props.user_verified.developer_name}
+              verified_count={props.building.verified.developer_name}
+              />
           <MultiDataEntry
               title={dataFields.developer_source_link.title}
               slug="developer_source_link"
