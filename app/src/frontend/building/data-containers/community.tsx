61166726c3181dd9728252960ca6323680e89e26
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-06-24 15:54:25 +0100

    Revert "2nd attempt remove copy tool from community opinions"
    This reverts commit a960bc7cb5c6418010c531ebfb0b776f241b74a9.

--- app/src/frontend/building/data-containers/community.tsx
+++ app/src/frontend/building/data-containers/community.tsx
@@ -35,7 +35,7 @@ const CommunityView: React.FunctionComponent<CategoryViewProps> = (props) => {
 
                 onChange={props.onSaveChange}
                 mode={props.mode}
-
+                copy={props.copy}
 
             />
             <LogicalDataEntry
@@ -79,7 +79,7 @@ const CommunityView: React.FunctionComponent<CategoryViewProps> = (props) => {
 
                 onChange={props.onSaveChange}
                 mode={props.mode}
-
+                copy={props.copy}
             />
         </div>
 
