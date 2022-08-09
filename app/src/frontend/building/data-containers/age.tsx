e573574d5cd3247d81dfa3d103758a608c988241
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-08-09 15:05:36 +0100

    add EditHistoryLatest

--- app/src/frontend/building/data-containers/age.tsx
+++ app/src/frontend/building/data-containers/age.tsx
@@ -8,7 +8,7 @@ import TextboxDataEntry from '../data-components/textbox-data-entry';
 import Verification from '../data-components/verification';
 import YearDataEntry from '../data-components/year-data-entry';
 import withCopyEdit from '../data-container';
-import { EditHistory } from '../edit-history/edit-history';
+import { EditHistoryLatest } from '../edit-history/edit-history';
 
 import { CategoryViewProps } from './category-view-props';
 
@@ -23,7 +23,7 @@ const AgeView: React.FunctionComponent<CategoryViewProps> = (props) => {
        ){
       return (
           <Fragment>
-              <EditHistory
+              <EditHistoryLatest
                   building={props.building}
               />
               <YearDataEntry
@@ -97,7 +97,7 @@ const AgeView: React.FunctionComponent<CategoryViewProps> = (props) => {
       };
     return (
         <Fragment>
-            <EditHistory
+            <EditHistoryLatest
                 building={props.building}
             />
             <YearDataEntry
