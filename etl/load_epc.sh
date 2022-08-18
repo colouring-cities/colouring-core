9dd395402a835e7b558f6f81598f6aba59e02b26
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-08-18 14:34:37 +0100

    allow for 'INVALID!' energy rating

--- etl/load_epc.sh
+++ etl/load_epc.sh
@@ -4,7 +4,7 @@ psql -c "DROP TABLE IF EXISTS epc;"
 psql -c "
 CREATE TABLE epc (
     index integer,
-    current_energy_rating char(1),
+    current_energy_rating varchar(8),
     lodgement_date timestamp,
     floor_level integer,
     construction_age_band varchar,
