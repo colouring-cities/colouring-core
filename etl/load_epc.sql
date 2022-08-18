4f80fbad898aaa7b3e5e8ca4b114e970dc7c7182
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-08-18 11:41:09 +0100

    add extra column

--- etl/load_epc.sql
+++ etl/load_epc.sql
@@ -8,10 +8,11 @@ CREATE TABLE epc (
     floor_level integer,
     construction_age_band varchar,
     uprn integer,
+    epcdatafromfile varchar,
     toid varchar
 );
 
 -- Read in the EPC data
-COPY epc (CURRENT_ENERGY_RATING, LODGEMENT_DATE, FLOOR_LEVEL, CONSTRUCTION_AGE_BAND, UPRN, TOID)
+COPY epc (CURRENT_ENERGY_RATING, LODGEMENT_DATE, FLOOR_LEVEL, CONSTRUCTION_AGE_BAND, UPRN, EPCDatafromFile, TOID)
 FROM 'gla-epc-subset.csv'
 WITH (FORMAT csv, HEADER true);
