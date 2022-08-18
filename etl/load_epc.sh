070f1f04f3986a641f98368743e62c70ed1bd521
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-08-18 14:09:50 +0100

    convert to bash

---
+++ etl/load_epc.sh
@@ -0,0 +1,22 @@
+psql -c "DROP TABLE IF EXISTS epc;"
+
+# Create EPC data table
+psql -c "
+CREATE TABLE epc (
+  
+    current_energy_rating char(1),
+    lodgement_date timestamp,
+    floor_level integer,
+    construction_age_band varchar,
+    uprn integer,
+    epcdatafromfile varchar,
+    toid varchar
+);
+"
+
+# Read in the EPC data
+psql -c "
+\copy epc (CURRENT_ENERGY_RATING, LODGEMENT_DATE, FLOOR_LEVEL, CONSTRUCTION_AGE_BAND, UPRN, EPCDatafromFile, TOID)
+FROM 'gla-epc-subset.csv'
+WITH (FORMAT csv, HEADER true);
+"
