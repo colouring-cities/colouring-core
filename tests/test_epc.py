bce6b11f16ad6a7ac30154e64b1ef971c72574d6
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-08-26 14:44:43 +0100

    refactor

--- tests/test_epc.py
+++ tests/test_epc.py
@@ -6,7 +6,5 @@ def test_floor_level_to_int():
     """Test that differently formatted floors can correctly converted."""
     test_levels = ['01', '02' '1st', '2nd', '3rd', '4th', '1', '2', '0', 'Ground', 'NODATA!', 'mid floor', 'Basement']
     expected = [1, 2, 1, 2, 3, 4, 1, 2, 0, 0, None, 'mid floor', 'Basement']
-    i = 0
-    for lvl in test_levels:
-        assert floor_level_to_int(lvl) == expected[i]
-        i+=1
+    for lvl, ex in zip(test_levels, expected):
+        assert floor_level_to_int(lvl) == ex
