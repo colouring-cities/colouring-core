a177c0da91d1998ae15c95ab09fe06495e4e1b2d
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-08-26 15:25:29 +0100

    basement rule

--- etl/epc_cleaning_functions.py
+++ etl/epc_cleaning_functions.py
@@ -2,6 +2,8 @@ def floor_level_to_int(lvl):
     """Convert differently formatted floor level strings to ints."""
     if 'Ground' in lvl or 'ground' in lvl:
         lvl = 0
+    elif 'basement' in lvl or 'Basement' in lvl:
+        lvl = -1
     elif lvl == 'NODATA!':
         lvl = None
     elif lvl[0] == '0' and lvl != '0':
--- tests/test_epc.py
+++ tests/test_epc.py
@@ -5,6 +5,6 @@ from etl import floor_level_to_int
 def test_floor_level_to_int():
     """Test that differently formatted floors can correctly converted."""
     test_levels = ['01', '02', '1st', '2nd', '3rd', '4th', '1', '2', '0', 'Ground', 'NODATA!', 'mid floor', 'Basement', 'ground floor']
-    expected = [1, 2, 1, 2, 3, 4, 1, 2, 0, 0, None, 'mid floor', 'Basement', 0]
+    expected = [1, 2, 1, 2, 3, 4, 1, 2, 0, 0, None, 'mid floor', -1, 0]
     for lvl, ex in zip(test_levels, expected):
         assert floor_level_to_int(lvl) == ex
