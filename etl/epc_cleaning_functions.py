6d83ffadc4ed508c16ecec112d2f37ed7a4c9d50
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-08-26 15:41:13 +0100

    assume floor level is 0 by default

--- etl/epc_cleaning_functions.py
+++ etl/epc_cleaning_functions.py
@@ -7,7 +7,7 @@ def floor_level_to_int(lvl):
     """
     lvl = lvl.replace('or above', '')
     lvl = lvl.replace('+', '')
-    if 'Ground' in lvl or 'ground' in lvl:
+    if 'Ground' in lvl or 'ground' in lvl or lvl == 'NODATA!':
         lvl = 0
     elif 'basement' in lvl or 'Basement' in lvl:
         lvl = -1
@@ -15,14 +15,10 @@ def floor_level_to_int(lvl):
         lvl = 1
     elif lvl == 'top floor':
         lvl = 2
-    elif lvl == 'NODATA!':
-        lvl = None
     elif lvl[0] == '0' and lvl != '0':
         lvl = lvl[1]
     else:
         ordinals = ['st', 'nd', 'rd', 'th']
         for ordinal in ordinals:
             lvl = lvl.replace(ordinal, '')
-    if lvl == None:
-        return None
     return int(lvl)
