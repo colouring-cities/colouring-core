5638524892b94a54d774e5ab82d83959340c2dd3
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-08-26 14:38:00 +0100

    test passes

--- tests/test_epc.py
+++ tests/test_epc.py
@@ -6,8 +6,10 @@ def test_floor_level_to_int():
     """Test that differently formatted floors can correctly converted."""
     test_levels = ['01', '02' '1st', '2nd', '3rd', '4th', '1', '2', '0', 'Ground', 'NODATA!', 'mid floor', 'Basement']
     expected = [1, 2, 1, 2, 3, 4, 1, 2, 0, 0, None, 'mid floor', 'Basement']
-    real_outputs = list(map(floor_level_to_int, levels))
     i = 0
-    for val in real_outputs:
-        assert val == expected[i]
+    for lvl in test_levels:
+        try:
+            assert floor_level_to_int(lvl) == expected[i]
+        except AssertionError:
+            print(lvl)
         i+=1
