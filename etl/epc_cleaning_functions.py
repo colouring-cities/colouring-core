4e249d2e9b06c9b9313547e35528c95abbd8d91a
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-08-26 15:35:23 +0100

    cover all floor level variations

--- etl/epc_cleaning_functions.py
+++ etl/epc_cleaning_functions.py
@@ -1,9 +1,15 @@
 def floor_level_to_int(lvl):
     """Convert differently formatted floor level strings to ints."""
+    lvl = lvl.replace('or above', '')
+    lvl = lvl.replace('+', '')
     if 'Ground' in lvl or 'ground' in lvl:
         lvl = 0
     elif 'basement' in lvl or 'Basement' in lvl:
         lvl = -1
+    elif lvl == 'mid floor':
+        lvl = 1
+    elif lvl == 'top floor':
+        lvl = 2
     elif lvl == 'NODATA!':
         lvl = None
     elif lvl[0] == '0' and lvl != '0':
@@ -12,8 +18,6 @@ def floor_level_to_int(lvl):
         ordinals = ['st', 'nd', 'rd', 'th']
         for ordinal in ordinals:
             lvl = lvl.replace(ordinal, '')
-    try:
-        lvl = int(lvl)
-    except:
-        pass
-    return lvl
+    if lvl == None:
+        return None
+    return int(lvl)
