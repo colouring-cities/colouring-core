c0ed161fad457636e61e923a22616cdeb6b8d978
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-08-26 15:52:01 +0100

    refactor so int always returned

--- etl/epc_cleaning_functions.py
+++ etl/epc_cleaning_functions.py
@@ -5,20 +5,25 @@ def floor_level_to_int(lvl):
        for the floor for each building automatically populated by EPC data.
        Incorrect assumptions can be updated later via the Colouring London interface.
     """
+    ordinals = ['st', 'nd', 'rd', 'th']
     lvl = lvl.replace('or above', '')
     lvl = lvl.replace('+', '')
-    if 'Ground' in lvl or 'ground' in lvl or lvl == 'NODATA!':
-        lvl = 0
-    elif 'basement' in lvl or 'Basement' in lvl:
-        lvl = -1
-    elif lvl == 'mid floor':
-        lvl = 1
-    elif lvl == 'top floor':
-        lvl = 2
-    elif lvl[0] == '0' and lvl != '0':
-        lvl = lvl[1]
-    else:
-        ordinals = ['st', 'nd', 'rd', 'th']
-        for ordinal in ordinals:
-            lvl = lvl.replace(ordinal, '')
-    return int(lvl)
+    try:
+        return int(lvl)
+    except ValueError:
+        if 'Ground' in lvl or 'ground' in lvl:
+            lvl = 0
+        elif 'basement' in lvl or 'Basement' in lvl:
+            lvl = -1
+        elif lvl == 'mid floor':
+            lvl = 1
+        elif lvl == 'top floor':
+            lvl = 2
+        elif lvl[0] == '0' and lvl != '0':
+            lvl = lvl[1]
+        elif any(ordinal in lvl for ordinal in ordinals):
+            for ordinal in ordinals:
+                lvl = lvl.replace(ordinal, '')
+        else:
+            lvl = 0
+        return int(lvl)
