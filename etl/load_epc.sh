ed06d348a19833008897fea9c697b2cb5e93483b
Ed Chalstrey <edwardchalstrey@gmail.com>
2022-08-31 11:12:54 +0100

    change construction_age_band to smallint to match date_year

--- etl/load_epc.sh
+++ etl/load_epc.sh
@@ -1,13 +1,15 @@
 psql -c "DROP TABLE IF EXISTS epc;"
 
 # Create EPC data table
+## construction_age_band should match date_year in buildings table
+## uprn and toid can also be linked to building table
 psql -c "
 CREATE TABLE epc (
     index integer,
     current_energy_rating char(1),
     lodgement_date timestamp,
     floor_level integer,
-    construction_age_band varchar,
+    construction_age_band smallint,
     uprn bigint,
     epc_data_from_file varchar,
     toid varchar
