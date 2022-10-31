0f689217c3be8adb016747284398849647e7ccde
Mateusz Konieczny <matkoniecz@gmail.com>
2022-10-31 08:49:13 +0100

    change data source

--- app/src/frontend/building/data-components/planning-data-entry.tsx
+++ app/src/frontend/building/data-components/planning-data-entry.tsx
@@ -16,6 +16,10 @@ const LongText = ({ content,limit}) => {
   const showMore = () => setShowAll(true);
   const showLess = () => setShowAll(false);
 
+  if (content == null) {
+    return <div>{MissingData}</div>
+  }
+
   if (content.length <= limit) {
     return <div>{content}</div>
   }
@@ -36,6 +40,12 @@ const LongText = ({ content,limit}) => {
 
 const Disclaimer = () => { return <Fragment><div><i><u>Disclaimer</u>: these data are currently incomplete and also often do not provide information on minor alterations. For comprehensive information on all applications please visit the local authorities' planning websites.</i></div></Fragment> }
 
+const MissingData = "not available"
+
+function ShowIfAvailable(data) {
+  return <>{data ? data.toString() : MissingData }</>
+}
+
 const PlanningDataOfficialDataEntry: React.FC<PlanningDataOfficialDataEntryProps> = (props) => {
 
     const data = props.value || [];
@@ -54,13 +64,13 @@ const PlanningDataOfficialDataEntry: React.FC<PlanningDataOfficialDataEntryProps
             <Fragment>
                 <div><i>Planning application status is streamed using live data uploaded by local authorities to the <a href={data[0]["data_source_link"]}>{data[0]["data_source"]}</a>.</i></div>
                 <br/>
-                <div><b>Current planning application status for this site:</b> {data[0]["status"]}</div>
-                <div><b>Planning application ID:</b> {data[0]["planning_application_id"]}</div>
-                <div><b>Date registered by the planning authority (validation date)</b>: {data[0]["registered_with_local_authority_date"]}</div>
-                <div><b>Decision date</b>: {data[0]["decision_date"].toString()}</div>
-                <div><b>Planning application link</b>: TODO move here</div>
-                <div><b>Description of proposed work</b>: <LongText content = {data[0]["description"]} limit = {400}/></div>
-                <div><b>Most recent update by data provider:</b> {data[0]["decision_date"]}</div>
+                <div><b>Current planning application status for this site:</b> {ShowIfAvailable(data[0]["status"])}</div>
+                <div><b>Planning application ID:</b> {ShowIfAvailable(data[0]["planning_application_id"])}</div>
+                <div><b>Date registered by the planning authority (validation date)</b>: {ShowIfAvailable(data[0]["registered_with_local_authority_date"])}</div>
+                <div><b>Decision date</b>: {ShowIfAvailable(data[0]["decision_date"])}</div>
+                <div><b>Planning application link</b>: {ShowIfAvailable(data[0]["planning_application_link"])}</div>
+                <div><b>Description of proposed work</b>: {data[0]["description"] ? <LongText content = {data[0]["description"]} limit = {400}/> : MissingData}</div>
+                <div><b>Most recent update by data provider:</b> {ShowIfAvailable(data[0]["decision_date"])}</div>
                 <br/>
                 <Disclaimer />
             </Fragment>
---
+++ etl/planning_data/obtain_livestream_data_and_load_into_database.py
@@ -0,0 +1,175 @@
+import requests
+import json
+import json
+import datetime
+import psycopg2
+import os
+
+
+def main():
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute("TRUNCATE planning_data")
+
+    downloaded = 0
+    last_sort = None
+    search_after = []
+    while True:
+        data = query(search_after).json()
+        load_data_into_database(cursor, data)
+        for entry in data['hits']['hits']:
+            downloaded += 1
+            last_sort = entry['sort']
+        print("downloaded", downloaded, "last_sort", last_sort, "previous", search_after)
+        if search_after == last_sort:
+            break
+        search_after = last_sort
+    connection.commit()
+
+
+
+def load_data_into_database(cursor, data):
+    if "timed_out" not in data:
+        print(json.dumps(data, indent = 4))
+        print("timed_out field missing in provided data")
+    else:
+        if data['timed_out']:
+            raise Exception("query getting livestream data has failed")
+    for entry in data['hits']['hits']:
+        try:
+            description = None
+            if entry['_source']['description'] != None:
+                description = entry['_source']['description'].strip()
+            application_id = entry['_source']['id']
+            decision_date = parse_date_string_into_datestring(entry['_source']['decision_date'])
+            last_synced_date = parse_date_string_into_datestring(entry['_source']['last_synced'])
+            uprn = entry['_source']['uprn']
+            status = process_status(entry['_source']['status'])
+            if uprn == None:
+                continue
+            try:
+                uprn = int(uprn)
+            except ValueError as e:
+                print(e)
+                continue
+            entry = {
+                "description": description,
+                "decision_date": decision_date,
+                "last_synced_date": last_synced_date,
+                "application_id": application_id,
+                "application_url": entry['_source']['url_planning_app'],
+                "registered_with_local_authority_date": parse_date_string_into_datestring(entry['_source']['valid_date']),
+                "uprn": uprn,
+                "status": status,
+                "data_source": "Greater London Authority's Planning London DataHub",
+                "data_source_link": "https://data.london.gov.uk/dataset/planning-london-datahub?_gl=1%2aprwpc%2a_ga%2aMzQyOTg0MjcxLjE2NTk0NDA4NTM", # TODO test
+                }
+            insert_entry(cursor, entry)
+        except TypeError as e:
+            print()
+            print()
+            print()
+            print(e)
+            print()
+            print(json.dumps(entry, indent = 4))
+
+def query(search_after):
+    headers = {
+        'X-API-AllowRequest': os.environ['PLANNNING_DATA_API_ALLOW_REQUEST_CODE'],
+        # Already added when you pass json= but not when you pass data=
+        # 'Content-Type': 'application/json',
+    }
+    json_data = {
+        'size': 10000,
+        'sort': [
+            {
+                'last_updated': {
+                    'order': 'desc',
+                    'unmapped_type': 'boolean',
+                },
+            },
+        ],
+        'stored_fields': [
+            '*',
+        ],
+        '_source': {
+            'excludes': [],
+        },
+        'query': {
+            'bool': {
+                'must': [
+                    {
+                        'range': {
+                            'valid_date': {
+                                'gte': '01/01/1021',
+                            },
+                        },
+                    },
+                ],
+            },
+        },
+    }
+
+    if search_after != []:
+        json_data['search_after'] = search_after
+
+    print(json_data)
+    return requests.post('https://planningdata.london.gov.uk/api-guest/applications/_search', headers=headers, json=json_data)
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.environ['PGHOST'],
+        dbname=os.environ['PGDATABASE'],
+        user=os.environ['PGUSER'],
+        password=os.environ['PGPASSWORD']
+    )
+
+def filepath():
+    return os.path.dirname(os.path.realpath(__file__)) + os.sep + "data.json"
+
+def insert_entry(cursor, e):
+    elements = []
+    application_url = None
+    if e["application_url"] != None:
+        application_url = "'" + e["application_url"] + "'"
+    cursor.execute('''INSERT INTO
+            planning_data (planning_application_id, planning_application_link, description, registered_with_local_authority_date, decision_date, last_synced_date, status, data_source, data_source_link, uprn)
+        VALUES
+            (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    ''', (e["application_id"], application_url, e["description"], e["registered_with_local_authority_date"], e["decision_date"], e["last_synced_date"], e["status"], e["data_source"], e["data_source_link"], e["uprn"]))
+
+def parse_date_string_into_datestring(incoming):
+    if incoming == None:
+        return None
+    date = None
+    try:
+        date = datetime.datetime.strptime(incoming, "%d/%m/%Y") # '21/07/2022'
+    except ValueError:
+        date = datetime.datetime.strptime(incoming, "%Y-%m-%dT%H:%M:%S.%fZ") # '2022-08-08T20:07:22.238Z'
+    return datetime.datetime.strftime(date, "%Y-%m-%d")
+
+def process_status(status):
+    """return None if status is invalid"""
+    if status in ["Application Under Consideration", "Application Received"]:
+        status = "Submitted"
+    if status in ["Refused", "Refusal", "Refusal (P)", "Application Invalid", "Insufficient Fee"]:
+        status = "Rejected"
+    if status == "Appeal Received":
+        status = "Appeal In Progress"
+    if status == "Completed":
+        status = "Approved"
+    if status in [None, "NOT_MAPPED"]:
+        status = "Unknown"
+    if (status in ["Submitted", "Approved", "Rejected", "Appeal In Progress", "Withdrawn", "Unknown"]):
+        return status
+    print("Unexpected status " + status)
+    if status not in ["No Objection to Proposal (OBS only)", "Objection Raised to Proposal (OBS only)", "Not Required", "Unknown", "Lapsed", "SECS", "Comment Issued", "ALL DECISIONS ISSUED", "Closed", "Declined to Determine"]:
+        print("New unexpected status " + status)
+    status_length_limit = 50 # see migrations/033.planning_livestream_data.up.sql
+    if len(status) > 50:
+        print("Status was too long and was skipped:", status)
+        return None
+    return status
+
+if __name__ == '__main__':
+    main()
