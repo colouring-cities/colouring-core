f3aaeddb96a564003a6442e8a1a05fe97f778537
Mateusz Konieczny <matkoniecz@gmail.com>
2022-11-13 14:00:47 +0100

    more visible conservation area layer

--- app/src/frontend/map/layers/conservation-boundary-layer.tsx
+++ app/src/frontend/map/layers/conservation-boundary-layer.tsx
@@ -17,7 +17,7 @@ export function ConservationAreaBoundaryLayer({enablement}: {enablement: Conserv
         <GeoJSON 
         attribution='Conservation areas by <a href=http://www.bedfordpark.net/leo/planning/>Leo Hall</a> on CC-BY 4.0 licence, contains data under the Open Government Licence v.3.0'
         data={boundaryGeojson}
-        style={{color: '#9400d3', fill: true, weight: 1, opacity: 0.6}}
+        style={{color: '#8a2be2', fill: true, weight: 1, opacity: 0.8}}
     />;
     } else if (enablement == "disabled") {
         return <div></div>
