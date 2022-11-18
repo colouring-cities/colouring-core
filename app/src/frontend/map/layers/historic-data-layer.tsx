8f1a03222cb3e8e7c88161398fdd95fff7221fd4
Mateusz Konieczny <matkoniecz@gmail.com>
2022-11-18 17:26:35 +0100

    new historic map example

--- app/src/frontend/map/layers/historic-data-layer.tsx
+++ app/src/frontend/map/layers/historic-data-layer.tsx
@@ -5,8 +5,8 @@ import { HistoricDataEnablementState } from '../../config/map-config';
 export function HistoricDataLayer({enablement}: {enablement: HistoricDataEnablementState}) {
     if(enablement == "enabled") {
         return <TileLayer
-            url="https://maps.georeferencer.com/georeferences/53c1ccd0-c169-5f9e-a850-dd34c066c369/2019-10-01T08:40:08.006175Z/map/{z}/{x}/{y}.png?key=xkWAs4LtX8CmpSTUqF9M"
-            attribution='&copy; ???????????????????????????????????????'
+            url="https://mapseries-tilesets.s3.amazonaws.com/london_1890s/{z}/{x}/{y}.png"
+            attribution='&copy; CC BY 4.0 - Reproduced with the permission of the <a href="https://maps.nls.uk/">National Library of Scotland</a>'
         />
 	} else {
         return null;
