97f8d9cf93ec5667fd79f0ff5d7e41e069752acd
Mateusz Konieczny <matkoniecz@gmail.com>
2022-12-06 17:02:19 +0100

    tweak housing colour

--- app/src/frontend/map/layers/housing-boundary-layer.tsx
+++ app/src/frontend/map/layers/housing-boundary-layer.tsx
@@ -17,7 +17,7 @@ export function HousingBoundaryLayer({enablement}: {enablement: HousingEnablemen
         <GeoJSON 
         attribution="Housing Zones from <a href=https://data.london.gov.uk/dataset/housing_zones>London Datastore</a>. The boundaries are based on Ordnance Survey mapping and the data is published under Ordnance Survey's 'presumption to publish'. Contains OS data © Crown copyright and database rights 2019. Greater London Authority - Contains public sector information licensed under the <a href=https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/>Open Government Licence v3.0</a>'"
         data={boundaryGeojson}
-        style={{color: '#00f', fill: true, weight: 1, opacity: 0.6}}
+        style={{color: '#FF8000', fill: true, weight: 1, opacity: 0.6}}
     />;
     } else if (enablement == "disabled") {
         return <div></div>
