a94c5da927d8628c7de98f868a18651eb912f657
Mateusz Konieczny <matkoniecz@gmail.com>
2022-12-07 14:25:50 +0100

    describe action on buttons

--- app/src/frontend/building/data-containers/planning.tsx
+++ app/src/frontend/building/data-containers/planning.tsx
@@ -150,7 +150,7 @@ const PlanningView: React.FunctionComponent<CategoryViewProps> = (props) => {
             tooltip={"GLA official description: \"All areas with more than a 1 in 1,000 annual probability of either river or sea flooding.\""}
             />
             <button className="map-switcher-inline btn btn-outline btn-outline-dark" onClick={floodSwitchOnClick}>
-                {(flood === 'enabled')? 'Click to hide layer' : 'Click to see the data mapped'}
+                {(flood === 'enabled')? 'Click to hide Flood Zones' : 'Click to see Flood Zones mapped'}
             </button>
             <LogicalDataEntry
             title="Is the building in a Housing Zone?"
@@ -160,7 +160,7 @@ const PlanningView: React.FunctionComponent<CategoryViewProps> = (props) => {
             tooltip={"GLA official description: \"Housing zones are areas funded by the Mayor and government to attract developers and relevant partners to build new homes.\""}
             />
             <button className="map-switcher-inline btn btn-outline btn-outline-dark" onClick={housingSwitchOnClick}>
-                {(housing === 'enabled')? 'Click to hide layer' : 'Click to see the data mapped'}
+                {(housing === 'enabled')? 'Click to hide Housing Zones' : 'Click to see Housing Zones mapped'}
             </button>
             <LogicalDataEntry
             title="Is the building in a Creative Enterprise Zone?"
@@ -170,7 +170,7 @@ const PlanningView: React.FunctionComponent<CategoryViewProps> = (props) => {
             tooltip={"GLA official description: \"Creative Enterprise Zones are a new Mayoral initiative to designate areas of London where artists and creative businesses can find permanent affordable space to work; are supported to start-up and grow; and where local people are helped to learn creative sector skills and find new jobs.\""}
             />
             <button className="map-switcher-inline btn btn-outline btn-outline-dark" onClick={creativeSwitchOnClick}>
-                {(creative === 'enabled')? 'Click to hide layer' : 'Click to see the data mapped'}
+                {(creative === 'enabled')? 'Click to hide Creative Enterprise Zones' : 'Click to see Creative Enterprise Zones'}
             </button>
             <LogicalDataEntry
             title="Is the building within a Protected Vista?"
@@ -180,7 +180,7 @@ const PlanningView: React.FunctionComponent<CategoryViewProps> = (props) => {
             tooltip={"GLA official description: \"The Protected Vistas are established in the London Plan with more detailed guidance provided in the London View Management Framework (LVMF). The London Plan seeks to protect the significant views which help to define London, including the panoramas, linear views and townscape views in this layer.\""}
             />
             <button className="map-switcher-inline btn btn-outline btn-outline-dark" onClick={vistaSwitchOnClick}>
-                {(vista === 'enabled')? 'Click to hide layer' : 'Click to see the data mapped'}
+                {(vista === 'enabled')? 'Click to hide Protected Vistas' : 'Click to see Protected Vistas'}
             </button>
             {/*
                 <DataEntry
