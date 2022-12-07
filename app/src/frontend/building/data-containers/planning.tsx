dcc277bee5c400f81b0184e5687e280d48110ef7
Mateusz Konieczny <matkoniecz@gmail.com>
2022-12-07 16:10:05 +0100

    tweak descriptions again

--- app/src/frontend/building/data-containers/planning.tsx
+++ app/src/frontend/building/data-containers/planning.tsx
@@ -122,7 +122,7 @@ const PlanningView: React.FunctionComponent<CategoryViewProps> = (props) => {
                 />
             </DataEntryGroup>
             <DataEntryGroup name="Possible future applications (crowdsourced data)" collapsed={true} >
-                <InfoBox>You can click and colour any other building on the map as well. To add your opinion on how well this building works for the community, please visit the <Link to={communityLinkUrl}>Community</Link> section.</InfoBox>
+                <InfoBox>Click and colour buildings here if you think that there will be a future planning application. To add your opinion on how well this building works for the community, please visit the <Link to={communityLinkUrl}>Community</Link> section.</InfoBox>
                 <UserOpinionEntry
                     slug='community_expected_planning_application'
                     title={buildingUserFields.community_expected_planning_application.title}
