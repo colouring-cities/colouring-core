3ac50e254960bad33a7c7fdb818c4fd829b6e527
Mateusz Konieczny <matkoniecz@gmail.com>
2022-12-08 19:30:34 +0100

    fix night map style

--- app/src/frontend/components/info-box.tsx
+++ app/src/frontend/components/info-box.tsx
@@ -4,7 +4,7 @@ interface InfoBoxProps {
     msg?: string;
     // https://react-bootstrap.github.io/components/alerts/
     // predefined valid values
-    type?: 'info' | 'warning' | 'success' | 'danger'
+    type?: 'info' | 'warning' | 'success' | 'danger' | 'dark'
 }
 
 const InfoBox: React.FC<InfoBoxProps> = ({msg, children, type = 'info'}) => (
