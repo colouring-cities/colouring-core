14c2ca92d86964cbfdf3b80e8e4d6158adec622d
Mateusz Konieczny <matkoniecz@gmail.com>
2022-12-14 12:14:49 +0100

    tweak styling of conservation boundary

--- app/src/frontend/map/layers/conservation-boundary-layer.tsx
+++ app/src/frontend/map/layers/conservation-boundary-layer.tsx
@@ -18,7 +18,7 @@ export function ConservationAreaBoundaryLayer({}) {
         <GeoJSON 
         attribution='Conservation areas by <a href=http://www.bedfordpark.net/leo/planning/>Leo Hall</a> on <a href=https://creativecommons.org/licenses/by/4.0/legalcode>CC-BY 4.0 licence</a>, contains data under <a href=https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/>the Open Government Licence v.3.0</a>'
         data={boundaryGeojson}
-        style={{color: '#cd7090', fill: true, weight: 1, opacity: 1, fillOpacity: 0.8}}
+        style={{color: '#cd7090', fill: true, weight: 3, opacity: 1, fillOpacity: 0.4}}
     />;
     } else if (conservation == "disabled") {
         return <div></div>
