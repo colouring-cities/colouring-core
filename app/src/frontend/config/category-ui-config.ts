5297f153e1a98e0142b5e6c4818ca73649ca763a
Mike Simpson <mike.simpson@ncl.ac.uk>
2023-05-16 17:16:20 +0100

    Rename files to match category titles

--- app/src/frontend/config/category-ui-config.ts
+++ app/src/frontend/config/category-ui-config.ts
@@ -1,17 +1,17 @@
 import { Category } from './categories-config';
 
-import AgeContainer from '../building/data-containers/age';
+import AgeContainer from '../building/data-containers/age-history';
 import CommunityContainer from '../building/data-containers/community';
 import ConstructionContainer from '../building/data-containers/construction';
 import ResilienceContainer from '../building/data-containers/resilience';
 import LocationContainer from '../building/data-containers/location';
 import PlanningContainer from '../building/data-containers/planning';
 import SizeContainer from '../building/data-containers/size';
-import StreetscapeContainer from '../building/data-containers/streetscape';
-import SustainabilityContainer from '../building/data-containers/sustainability';
+import StreetscapeContainer from '../building/data-containers/street-context';
+import SustainabilityContainer from '../building/data-containers/energy-performance';
 import TeamContainer from '../building/data-containers/team';
-import TypeContainer from '../building/data-containers/type';
-import UseContainer from '../building/data-containers/use';
+import TypeContainer from '../building/data-containers/typology';
+import UseContainer from '../building/data-containers/land-use';
 
 import { DataContainerType } from '../building/data-container';
 
