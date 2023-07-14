85b0821490a5b001f269dab7bef69daafcd0ce94
Mike Simpson <mike.simpson@ncl.ac.uk>
2023-07-14 13:35:53 +0100

    Add link to Open Manual (Configurable)
    Each project should configure this to point to their page on the open manual. (i.e. https://github.com/colouring-cities/manual/wiki/M3.-COLOURING-BRITAIN)

--- app/src/cc-config.ts
+++ app/src/cc-config.ts
@@ -1,8 +1,12 @@
+import { StringNullableChain } from "lodash";
+
 export interface CCConfig
 {
     cityName: string;                           // City name (i.e. "Colouring {City Name}")
     projectBlurb: string;                       // Description used on homepage
+    
     githubURL: string;                          // URL of the project's GitHub repository
+    manualURL: string;                          // Link to the project's page in the Open Manual (i.e. https://github.com/colouring-cities/manual/wiki/M3.-COLOURING-BRITAIN)
     privacyStatement: string;                   // Privacy statement, including where data is stored
     
     initialMapPosition: [number, number];       // Initial location of the map [latitude, longitude]
