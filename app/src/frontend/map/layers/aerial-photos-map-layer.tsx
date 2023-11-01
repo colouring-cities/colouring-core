65a3b7a3110da38376922540976156c48eaa086e
Mike Simpson <mike.simpson@ncl.ac.uk>
2023-11-01 12:46:19 +0000

    Include building footprint outlines on Aerial Photos layer

--- app/src/frontend/map/layers/aerial-photos-map-layer.tsx
+++ app/src/frontend/map/layers/aerial-photos-map-layer.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { TileLayer } from 'react-leaflet';
 import { useDisplayPreferences } from '../../displayPreferences-context';
+import { BuildingBaseLayerAllZoom } from './building-base-layer-all-zoom';
+import { BuildingDataLayer } from './building-data-layer';
 
 export function AerialPhotosMapLayer({revisionId}: {revisionId: string}) {
     const { aerialPhotosMap } = useDisplayPreferences();
@@ -10,6 +12,7 @@ export function AerialPhotosMapLayer({revisionId}: {revisionId: string}) {
                     url="https://geo.nls.uk/maps/air-photos-1250/{z}/{x}/{y}.png"
                     attribution='&copy; CC BY 4.0 - Reproduced with the permission of the <a href="https://maps.nls.uk/">National Library of Scotland</a>'
                 />
+                <BuildingBaseLayerAllZoom theme="night_outlines" />
             </>
 	} else {
         return null;
