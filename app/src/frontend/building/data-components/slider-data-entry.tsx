14d161b19e88668e6c217528eed60946e4b5db4a
Mike Simpson <mike.simpson@ncl.ac.uk>
2023-12-19 13:24:19 +0000

    Add "n/a" to sliders as default value
    Instead of 1 (min) being the default value, set it to 0 ("None"). Moving the slider to other values selects that value, moving it back to n/a clears it.
    
    May need updating in the future. Will only work where 0 is not a valid value. Might be problematic if "min" is greater than 1? But will do for neuroaesthetic scores

--- app/src/frontend/building/data-components/slider-data-entry.tsx
+++ app/src/frontend/building/data-components/slider-data-entry.tsx
@@ -19,6 +19,7 @@ interface SliderDataEntryProps extends BaseDataEntryProps {
 const SliderDataEntry: React.FunctionComponent<SliderDataEntryProps> = (props) => {
 
     const marks = {
+        ["None"]: <strong>n/a</strong>,
         [props.min]: <strong>{props.min}</strong>,
         [props.max]: <strong>{props.max}</strong>
     };
@@ -34,21 +35,24 @@ const SliderDataEntry: React.FunctionComponent<SliderDataEntryProps> = (props) =
             />
             <div className="slider-container">
                 <Slider
-                    value={props.value || props.min}
+                    value={props.value || 0}
+                    defaultValue={0}
                     max={props.max}
-                    min={props.min}
+                    min={0}
                     dots={props.dots}
                     marks={marks}
                     disabled={props.mode === 'view' || props.disabled}
                     onChange={
                         e => props.onChange(
                             props.slug, 
-                            typeof(e)=="number" ? e : e[0]
+                            typeof(e)=="number" ? (e?e:null) : e[0]
                         )
                     }
                 />
             </div>
-            <p className="slider-score">Your score: {props.value} <span className="slider-average">Average: {props.average} ({props.votes} votes)</span></p>
+            <p className="slider-score">
+                Your score: {props.value?props.value:"None"} <span className="slider-average">Average: {props.average} ({props.votes} votes)</span>
+            </p>
         </Fragment>
     );
 };
