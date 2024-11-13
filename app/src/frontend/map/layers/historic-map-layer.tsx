55f6e07def4e28b0d420a6b00a86522084881fda
Mateusz Konieczny <matkoniecz@gmail.com>
2024-11-13 12:26:24 +0100

    make attribution more clear

--- app/src/frontend/map/layers/historic-map-layer.tsx
+++ app/src/frontend/map/layers/historic-map-layer.tsx
@@ -8,7 +8,7 @@ export function HistoricMapLayer() {
         return <>
                 <TileLayer
                     url="https://mapseries-tilesets.s3.amazonaws.com/london_1890s/{z}/{x}/{y}.png"
-                    attribution='&copy; CC BY 4.0 - Reproduced with the permission of the <a href="https://maps.nls.uk/">National Library of Scotland</a>'
+                    attribution='historical map: &copy; CC BY 4.0 - Reproduced with the permission of the <a href="https://maps.nls.uk/">National Library of Scotland</a>'
                 />
             </>
 	} else {
