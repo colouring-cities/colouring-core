76c25482aa5f8efbd461b473c1f3414d2bdd1111
Mateusz Konieczny <matkoniecz@gmail.com>
2024-11-23 07:40:54 +0100

    clarify button mode

--- app/src/frontend/building/data-containers/age-history.tsx
+++ app/src/frontend/building/data-containers/age-history.tsx
@@ -520,11 +520,11 @@ const AgeHistoryView: React.FunctionComponent<CategoryViewProps> = (props) => {
                 }
                 {(historicData === "enabled") ? 
                     <button className={`map-switcher-inline enabled-state btn btn-outline btn-outline-dark ${darkLightTheme}`} onClick={switchToAgeMapStyle}>
-                        Click to hide the 1890s OS historical map with modern footprints.
+                        Click to hide the OS historical map with modern footprints and fill indicating survival status.
                     </button>
                 :
                     <button className={`map-switcher-inline disabled-state btn btn-outline btn-outline-dark ${darkLightTheme}`} onClick={switchToSurvivalDataStyle}>
-                        Click to show the 1890s OS historical map with modern footprints.
+                        Click to show the OS historical map with modern footprints and fill indicating survival status.
                     </button>
                 }
                 <SelectDataEntry
