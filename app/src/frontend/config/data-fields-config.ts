0baa6c8fb039b8ed28028d98f81d86f13400d34f
Mateusz Konieczny <matkoniecz@gmail.com>
2024-11-25 11:03:14 +0100

    now 'Current Building on Previously Unbuilt-on Site'

--- app/src/frontend/config/data-fields-config.ts
+++ app/src/frontend/config/data-fields-config.ts
@@ -1122,7 +1122,7 @@ export const dataFields = { /* eslint-disable @typescript-eslint/camelcase */
             "Same as Historical Map (Unchanged)",
             "Similar to Historical Map (Some Changes)",
             "Historical Building(s) Demolished",
-            "Current Building on Previous Green Space"
+            "Current Building on Previously Unbuilt-on Site"
         ],
         example: "",
     },
