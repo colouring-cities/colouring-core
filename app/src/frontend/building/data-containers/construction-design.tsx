a41935c0abbb84fb4b14044703f9663db782262b
Mike Simpson <mike.simpson@ncl.ac.uk>
2025-02-04 14:47:41 +0000

    Remove unused reference

--- app/src/frontend/building/data-containers/construction-design.tsx
+++ app/src/frontend/building/data-containers/construction-design.tsx
@@ -11,7 +11,7 @@ import withCopyEdit from '../data-container';
 
 import { CategoryViewProps } from './category-view-props';
 import { MultiDataEntry } from '../data-components/multi-data-entry/multi-data-entry';
-import { LogicalDataEntry, LogicalDataEntryYesOnly } from '../data-components/logical-data-entry/logical-data-entry';
+import { LogicalDataEntry } from '../data-components/logical-data-entry/logical-data-entry';
 
 /**
 * Construction & Design view/edit section
