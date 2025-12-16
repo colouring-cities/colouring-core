c935838d175407c876ce6950f2f48184d64f5cf6
Mateusz Konieczny <matkoniecz@gmail.com>
2025-12-16 10:28:01 +0100

    fix whitespace

--- app/src/frontend/map/world-heritage-sites-switcher.tsx
+++ app/src/frontend/map/world-heritage-sites-switcher.tsx
@@ -1,17 +1,16 @@
+import React from 'react';
 
-    import React from 'react';
+import './map-button.css';
+import { useDisplayPreferences } from '../displayPreferences-context';
 
-    import './map-button.css';
-    import { useDisplayPreferences } from '../displayPreferences-context';
-
-    export const WorldHeritageSitesSwitcher: React.FC<{}> = () => {
-        const { worldHeritageSites, worldHeritageSitesSwitch, darkLightTheme } = useDisplayPreferences();
-        return (
-            <form className={`map-button ${worldHeritageSites}-state ${darkLightTheme}`} onSubmit={worldHeritageSitesSwitch}>
-                <button className="btn btn-outline btn-outline-dark"
-                    type="submit">
-                    {(worldHeritageSites === 'enabled')? 'World Heritage Sites [on]' : 'World Heritage Sites [off]'}
-                </button>
-            </form>
-        );
-    }
+export const WorldHeritageSitesSwitcher: React.FC<{}> = () => {
+    const { worldHeritageSites, worldHeritageSitesSwitch, darkLightTheme } = useDisplayPreferences();
+    return (
+        <form className={`map-button ${worldHeritageSites}-state ${darkLightTheme}`} onSubmit={worldHeritageSitesSwitch}>
+            <button className="btn btn-outline btn-outline-dark"
+                type="submit">
+                {(worldHeritageSites === 'enabled')? 'World Heritage Sites [on]' : 'World Heritage Sites [off]'}
+            </button>
+        </form>
+    );
+}
